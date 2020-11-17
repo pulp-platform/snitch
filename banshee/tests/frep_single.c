7d3c4b1b3c4f1fc9ffd1c587e03fd6e25fbcbe84
Fabian Schuiki <fabian@schuiki.ch>
2020-11-17 19:23:37 +0100

    banshee: Comment out `frep.o` asm to run on baseline riscv gcc

--- banshee/tests/frep_single.c
+++ banshee/tests/frep_single.c
@@ -2,8 +2,8 @@ int main() {
     double x = 0;
     volatile register int N asm ("t0") = 4;
     asm volatile (
-        // ".word (0 << 20)|(5 << 15)|(1 << 7)|(0b0001011 << 0) \n" // frep t0, 1
-        "frep.o %2, 1 \n"
+        ".word (0 << 20)|(5 << 15)|(1 << 7)|(0b0001011 << 0) \n" // frep t0, 1
+        // "frep.o %2, 1 \n"
         "fadd.d %0, %0, %1 \n"
         : "+f"(x) : "f"(1.0), "r"(4)
     );
