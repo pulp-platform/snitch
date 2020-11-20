6b80a5c023ec2f6d383ed7d4bf2671ac63812aa8
Fabian Schuiki <fabian@schuiki.ch>
2020-11-20 13:12:34 +0100

    banshee: Add first impl of teams

---
+++ sw/snRuntime/src/team.h
@@ -0,0 +1,24 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+#pragma once
+#include "snrt.h"
+
+extern __thread struct snrt_team *_snrt_team_current;
+extern const uint32_t _snrt_team_size;
+
+struct snrt_team {
+    /// Pointer to the root team description of this cluster.
+    struct snrt_team_root *root;
+};
+
+struct snrt_team_root {
+    struct snrt_team base;
+    uint32_t global_core_base_hartid;
+    uint32_t global_core_num;
+    uint32_t cluster_idx;
+    uint32_t cluster_num;
+    uint32_t cluster_core_base_hartid;
+    uint32_t cluster_core_num;
+    // more stuff goes here
+};
