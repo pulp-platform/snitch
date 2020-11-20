9d6a8d3e55afe3c266748ff16bf9870507b8c7fa
Fabian Schuiki <fabian@schuiki.ch>
2020-11-20 13:07:23 +0100

    runtime: Enable LTO and more warnings

--- sw/snRuntime/toolchain.cmake
+++ sw/snRuntime/toolchain.cmake
@@ -2,7 +2,10 @@ set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
 set(CMAKE_CXX_COMPILER riscv32-unknown-elf-g++)
 set(CMAKE_OBJCOPY riscv32-unknown-elf-objcopy)
 set(CMAKE_OBJDUMP riscv32-unknown-elf-objdump)
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION True)
 
 add_compile_options(-march=rv32imafd)
 add_link_options(-march=rv32imafd -nostdlib -Wl,-Ttext-segment=0x80000000)
 # add_link_options(-Wl,--verbose)
+
+add_compile_options(-Wextra)
