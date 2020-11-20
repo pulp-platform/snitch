6c805241d865bf0cc34f588212ee15f37679dcb3
Fabian Schuiki <fabian@schuiki.ch>
2020-11-20 13:13:49 +0100

    runtime: Add simple team test

---
+++ sw/snRuntime/tests/team_global.c
@@ -0,0 +1,22 @@
+// Must run with `--base-hartid=3 --num-cores=4 --num-clusters=2`
+#include <snrt.h>
+
+static const struct {
+    uint32_t global_id;
+    uint32_t global_num;
+} table;
+
+int main() {
+    uint32_t hartid;
+    asm volatile ("csrr %0, mhartid" : "=r"(hartid));
+    uint32_t i = hartid - 3; // shift by base hartid
+
+    uint32_t errors = 0;
+    errors += (snrt_global_core_idx() != i);
+    errors += (snrt_global_core_num() != 8);
+    errors += (snrt_cluster_idx() != i / 4);
+    errors += (snrt_cluster_num() != 2);
+    errors += (snrt_cluster_core_idx() != i % 4);
+    errors += (snrt_cluster_core_num() != 4);
+    return errors;
+}
