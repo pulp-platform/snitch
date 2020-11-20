423aaa844f8338410f8eb6c21484819fa0847e42
Fabian Schuiki <fabian@schuiki.ch>
2020-11-20 13:08:11 +0100

    runtime: Add first minimal snrt.h

---
+++ sw/snRuntime/include/snrt.h
@@ -0,0 +1,19 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+#pragma once
+
+#include <stdint.h>
+
+extern void snrt_barrier();
+
+extern uint32_t snrt_global_core_idx();
+extern uint32_t snrt_global_core_num();
+extern uint32_t snrt_cluster_core_idx();
+extern uint32_t snrt_cluster_core_num();
+extern uint32_t snrt_cluster_compute_core_idx();
+extern uint32_t snrt_cluster_compute_core_num();
+extern uint32_t snrt_cluster_dm_core_idx();
+extern uint32_t snrt_cluster_dm_core_num();
+extern uint32_t snrt_cluster_idx();
+extern uint32_t snrt_cluster_num();
