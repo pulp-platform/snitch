ce9c986c1f9bbfbaf05a627b5941406209e978b8
Fabian Schuiki <fabian@schuiki.ch>
2020-11-23 15:26:31 +0100

    runtime: Create cmake directory, move toolchain and utils there

---
+++ sw/cmake/toolchain-gcc.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
+set(CMAKE_CXX_COMPILER riscv32-unknown-elf-g++)
+set(CMAKE_OBJCOPY riscv32-unknown-elf-objcopy)
+set(CMAKE_OBJDUMP riscv32-unknown-elf-objdump)
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION true)
+
+add_compile_options(-march=rv32imafd -mabi=ilp32d -mcmodel=medany -mno-fdiv -ffast-math -fno-builtin-printf -fno-common)
+add_link_options(-march=rv32imafd -mabi=ilp32d -nostdlib -Wl,-Ttext-segment=0x80000000)
+# add_link_options(-Wl,--verbose)
+
+link_libraries(-lm -lgcc)
+
+add_compile_options(-ffunction-sections)
+add_compile_options(-Wextra)
