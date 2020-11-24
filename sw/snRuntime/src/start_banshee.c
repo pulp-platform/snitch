5131b67b87d081a6dda29c75979453f8711b7b44
Fabian Schuiki <fabian@schuiki.ch>
2020-11-24 16:33:00 +0100

    runtime: Increase stack size to 2**10
    Otherwise even the simplest printf will already cause a stack overflow.

--- sw/snRuntime/src/start_banshee.c
+++ sw/snRuntime/src/start_banshee.c
@@ -9,6 +9,8 @@ extern const uint32_t _snrt_banshee_cluster_base_hartid;
 extern const uint32_t _snrt_banshee_cluster_num;
 extern const uint32_t _snrt_banshee_cluster_id;
 
+const uint32_t snrt_stack_size __attribute__((weak)) = 10;
+
 void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num, void *spm_start, void *spm_end, struct snrt_team_root *team) {
     team->base.root = team;
     team->global_core_base_hartid =
