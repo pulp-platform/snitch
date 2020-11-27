15a85b82fe57707986cc8e29617728f072391f78
Fabian Schuiki <fabian@schuiki.ch>
2020-11-27 18:59:22 +0100

    runtime: Add global/cluster memory to team struct

--- sw/snRuntime/include/snrt.h
+++ sw/snRuntime/include/snrt.h
@@ -14,6 +14,16 @@
 #define snrt_max(a, b) ((a) > (b) ? (a) : (b))
 #endif
 
+/// A slice of memory.
+typedef struct snrt_slice {
+    void *start;
+    void *end;
+} snrt_slice_t;
+
+static inline size_t snrt_slice_len(snrt_slice_t s) {
+    return s.end - s.start;
+}
+
 extern void snrt_barrier();
 
 extern uint32_t __attribute__((pure)) snrt_hartid();
@@ -27,5 +37,8 @@ extern uint32_t snrt_cluster_dm_core_idx();
 extern uint32_t snrt_cluster_dm_core_num();
 extern uint32_t snrt_cluster_idx();
 extern uint32_t snrt_cluster_num();
+
+extern snrt_slice_t snrt_global_memory();
+extern snrt_slice_t snrt_cluster_memory();
 
 extern void *snrt_memcpy(void *dst, const void *src, size_t n);
