f8c606ce558fa0f59b563b4bfc82565a543cfd59
Fabian Schuiki <fabian@schuiki.ch>
2020-12-14 10:25:28 +0100

    lint: Enable `#` assembyl comments for license lint

--- util/lowrisc_misc-linters/licence-checker/licence-checker.py
+++ util/lowrisc_misc-linters/licence-checker/licence-checker.py
@@ -131,8 +131,8 @@ COMMENT_CHARS = [
     # Software Files
     ([".c", ".c.tpl", ".h", ".h.tpl", ".cc", ".cpp"], SLASH_SLASH),  # C, C++
     ([".def"], SLASH_SLASH),  # C, C++ X-Include List Declaration Files
-    ([".S"], [SLASH_SLASH, SLASH_STAR]),  # Assembly (With Preprocessing)
-    ([".s"], SLASH_STAR),  # Assembly (Without Preprocessing)
+    ([".S"], [SLASH_SLASH, SLASH_STAR, HASH]),  # Assembly (With Preprocessing)
+    ([".s"], [SLASH_STAR, HASH]),  # Assembly (Without Preprocessing)
     ([".ld", ".ld.tpl"], SLASH_STAR),  # Linker Scripts
     ([".rs"], SLASH_SLASH),  # Rust
 
