aa2af60b891db5e8b9bdf7ac8780b04c2d90b4aa
Fabian Schuiki <fabian@schuiki.ch>
2020-12-14 10:23:33 +0100

    banshee: Add missing license headers

--- banshee/build/softfloat/platform.h
+++ banshee/build/softfloat/platform.h
@@ -1,3 +1,7 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 #define LITTLEENDIAN 1
 
 #define INLINE inline __attribute__((always_inline))
