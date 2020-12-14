565095eb65b30f97b72b112d5a81710edfb214e3
Fabian Schuiki <fabian@schuiki.ch>
2020-12-14 10:23:33 +0100

    banshee: Add missing license headers

--- banshee/build/softfloat/specialize.h
+++ banshee/build/softfloat/specialize.h
@@ -1,3 +1,7 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef specialize_h
 #define specialize_h 1
 
--- banshee/tests/matmul/gen_data.py
+++ banshee/tests/matmul/gen_data.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Copyright 2020 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
 import sys
