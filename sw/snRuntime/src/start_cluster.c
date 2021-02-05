a74e6cb1591157c4c39fdc56619f8c76faa49851
Fabian Schuiki <fabian@schuiki.ch>
2021-02-05 14:32:27 +0100

    runtime: Run clang-format

--- sw/snRuntime/src/start_cluster.c
+++ sw/snRuntime/src/start_cluster.c
@@ -33,7 +33,7 @@ struct putc_buffer_header {
 static volatile struct putc_buffer {
     struct putc_buffer_header hdr;
     char data[PUTC_BUFFER_LEN];
-} * const putc_buffer = (void *)&_end;
+} *const putc_buffer = (void *)&_end;
 
 void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
                      void *spm_start, void *spm_end,
@@ -47,8 +47,10 @@ void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
     team->cluster_num = 1;
     team->cluster_core_base_hartid = bootdata->hartid_base;
     team->cluster_core_num = bootdata->core_count;
-    team->global_mem.start = (void *)0x90000000; // TODO: Read this from bootdata
-    team->global_mem.end = (void *)0x100000000;  // TODO: Read this from bootdata
+    team->global_mem.start =
+        (void *)0x90000000;  // TODO: Read this from bootdata
+    team->global_mem.end =
+        (void *)0x100000000;  // TODO: Read this from bootdata
     team->cluster_mem.start = spm_start;
     team->cluster_mem.end = spm_end;
 
@@ -69,7 +71,8 @@ void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
 }
 
 uint32_t _snrt_barrier_reg_ptr() {
-    const struct snrt_cluster_bootdata *bd = _snrt_team_current->root->device_tree;
+    const struct snrt_cluster_bootdata *bd =
+        _snrt_team_current->root->device_tree;
     return bd->tcdm_end + 0x28;
 }
 
@@ -80,13 +83,14 @@ void snrt_putchar(char character) {
     volatile struct putc_buffer *buf = &putc_buffer[snrt_hartid()];
     buf->data[buf->hdr.size++] = character;
     if (buf->hdr.size == PUTC_BUFFER_LEN || character == '\n') {
-        buf->hdr.syscall_mem[0] = 64;                    // sys_write
-        buf->hdr.syscall_mem[1] = 1;                     // file descriptor (1 = stdout)
-        buf->hdr.syscall_mem[2] = (uintptr_t)&buf->data; // buffer
-        buf->hdr.syscall_mem[3] = buf->hdr.size;         // length
+        buf->hdr.syscall_mem[0] = 64;  // sys_write
+        buf->hdr.syscall_mem[1] = 1;   // file descriptor (1 = stdout)
+        buf->hdr.syscall_mem[2] = (uintptr_t)&buf->data;  // buffer
+        buf->hdr.syscall_mem[3] = buf->hdr.size;          // length
 
         tohost = (uintptr_t)buf->hdr.syscall_mem;
-        while (fromhost == 0);
+        while (fromhost == 0)
+            ;
         fromhost = 0;
 
         buf->hdr.size = 0;
--- sw/snRuntime/tests/printf_simple.c
+++ sw/snRuntime/tests/printf_simple.c
@@ -1,8 +1,8 @@
 // Copyright 2020 ETH Zurich and University of Bologna.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-#include <stdio.h>
 #include <snrt.h>
+#include <stdio.h>
 
 int main() {
     for (uint32_t i = 0; i < snrt_global_core_num(); i++) {
