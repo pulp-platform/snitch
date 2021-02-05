a3c6baf32919faecab37c836a9462d8bea943a3b
Fabian Schuiki <fabian@schuiki.ch>
2021-02-05 14:28:03 +0100

    runtime: Add simple cluster putchar impl
    Add a simple implementation of putchar for single-cluster systems which
    uses the HTIF to channel accesses to a host operating system.

--- sw/snRuntime/src/start_cluster.c
+++ sw/snRuntime/src/start_cluster.c
@@ -23,6 +23,18 @@ struct snrt_cluster_bootdata {
     uint32_t tcdm_end;
 };
 
+// Rudimentary string buffer for putc calls.
+extern uint32_t _end;
+#define PUTC_BUFFER_LEN (1024 - sizeof(size_t))
+struct putc_buffer_header {
+    size_t size;
+    uint64_t syscall_mem[8];
+};
+static volatile struct putc_buffer {
+    struct putc_buffer_header hdr;
+    char data[PUTC_BUFFER_LEN];
+} * const putc_buffer = (void *)&_end;
+
 void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
                      void *spm_start, void *spm_end,
                      const struct snrt_cluster_bootdata *bootdata,
@@ -49,6 +61,11 @@ void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
     team->cluster_mailbox = team->cluster_mem.end;
 
     _snrt_team_current = &team->base;
+
+    // Initialize the string buffer. This technically doesn't belong here, but
+    // the _snrt_init_team function is called once per thread before main, so
+    // it's as good a point as any.
+    putc_buffer[snrt_hartid()].hdr.size = 0;
 }
 
 uint32_t _snrt_barrier_reg_ptr() {
@@ -56,7 +73,22 @@ uint32_t _snrt_barrier_reg_ptr() {
     return bd->tcdm_end + 0x28;
 }
 
+extern uintptr_t volatile tohost, fromhost;
+
 // Provide an implementation for putchar.
 void snrt_putchar(char character) {
-    // *(volatile uint32_t *)0xF00B8000 = character;
+    volatile struct putc_buffer *buf = &putc_buffer[snrt_hartid()];
+    buf->data[buf->hdr.size++] = character;
+    if (buf->hdr.size == PUTC_BUFFER_LEN || character == '\n') {
+        buf->hdr.syscall_mem[0] = 64;                    // sys_write
+        buf->hdr.syscall_mem[1] = 1;                     // file descriptor (1 = stdout)
+        buf->hdr.syscall_mem[2] = (uintptr_t)&buf->data; // buffer
+        buf->hdr.syscall_mem[3] = buf->hdr.size;         // length
+
+        tohost = (uintptr_t)buf->hdr.syscall_mem;
+        while (fromhost == 0);
+        fromhost = 0;
+
+        buf->hdr.size = 0;
+    }
 }
