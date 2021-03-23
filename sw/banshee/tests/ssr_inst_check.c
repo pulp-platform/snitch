fb93f525564f65ca86135a9309df750edd8e7de6
Noah Huetter <noahhuetter@gmail.com>
2021-03-23 17:45:00 +0100

    banshee: Add SSR config test mixing immediate and register versions

--- sw/banshee/tests/ssr_inst_check.c
+++ sw/banshee/tests/ssr_inst_check.c
@@ -50,6 +50,30 @@ int main() {
         "beq    t1, t3, 4f\n"
         "addi   %[errs], %[errs], 1\n"  // increment error on mismatch
         "4:\n"
+
+        // ------------------------------------
+        // write and read to ssr1 stride2 with reg/imm mixed
+        // ------------------------------------
+        "li     t0, (0x1 | 8<<5)\n"  // address
+        "li     t1, 0x23456789\n"    // data
+        "scfgw  t1, t0\n"            // val, adr
+        "scfgri t3, (0x1 | 8<<5)\n"  // val, adr
+        // compare
+        "beq    t1, t3, 5f\n"
+        "addi   %[errs], %[errs], 1\n"  // increment error on mismatch
+        "5:\n"
+
+        // ------------------------------------
+        // write and read to ssr1 stride2 with reg/imm mixed
+        // ------------------------------------
+        "li     t0, (0x1 | 8<<5)\n"  // address
+        "li     t1, 0x42424242\n"    // data
+        "scfgwi t1, (0x1 | 8<<5)\n"  // val, adr
+        "scfgr  t3, t0\n"            // val, adr
+        // compare
+        "beq    t1, t3, 6f\n"
+        "addi   %[errs], %[errs], 1\n"  // increment error on mismatch
+        "6:\n"
         : [ errs ] "+r"(errs)::"t0", "t1", "t2", "t3");
     return errs;
 }
