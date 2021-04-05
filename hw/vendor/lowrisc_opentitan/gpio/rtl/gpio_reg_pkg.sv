2cff3928f7ced8afb1ef2b3acb267bc49120d4aa
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2021-04-05 11:24:02 +0200

    occamy: Add lowRISC GPIO

--- hw/vendor/lowrisc_opentitan/gpio/rtl/gpio_reg_pkg.sv
+++ hw/vendor/lowrisc_opentitan/gpio/rtl/gpio_reg_pkg.sv
@@ -6,13 +6,9 @@
 
 package gpio_reg_pkg;
 
-  // Address widths within the block
-  parameter int BlockAw = 6;
-
   ////////////////////////////
   // Typedefs for registers //
   ////////////////////////////
-
   typedef struct packed {
     logic [31:0] q;
   } gpio_reg2hw_intr_state_reg_t;
@@ -100,6 +96,7 @@ package gpio_reg_pkg;
     logic [31:0] q;
   } gpio_reg2hw_ctrl_en_input_filter_reg_t;
 
+
   typedef struct packed {
     logic [31:0] d;
     logic        de;
@@ -154,64 +151,60 @@ package gpio_reg_pkg;
     } mask;
   } gpio_hw2reg_masked_oe_upper_reg_t;
 
-  // Register -> HW type
+
+  ///////////////////////////////////////
+  // Register to internal design logic //
+  ///////////////////////////////////////
   typedef struct packed {
-    gpio_reg2hw_intr_state_reg_t intr_state; // [458:427]
-    gpio_reg2hw_intr_enable_reg_t intr_enable; // [426:395]
-    gpio_reg2hw_intr_test_reg_t intr_test; // [394:362]
-    gpio_reg2hw_direct_out_reg_t direct_out; // [361:329]
-    gpio_reg2hw_masked_out_lower_reg_t masked_out_lower; // [328:295]
-    gpio_reg2hw_masked_out_upper_reg_t masked_out_upper; // [294:261]
-    gpio_reg2hw_direct_oe_reg_t direct_oe; // [260:228]
-    gpio_reg2hw_masked_oe_lower_reg_t masked_oe_lower; // [227:194]
-    gpio_reg2hw_masked_oe_upper_reg_t masked_oe_upper; // [193:160]
-    gpio_reg2hw_intr_ctrl_en_rising_reg_t intr_ctrl_en_rising; // [159:128]
-    gpio_reg2hw_intr_ctrl_en_falling_reg_t intr_ctrl_en_falling; // [127:96]
-    gpio_reg2hw_intr_ctrl_en_lvlhigh_reg_t intr_ctrl_en_lvlhigh; // [95:64]
-    gpio_reg2hw_intr_ctrl_en_lvllow_reg_t intr_ctrl_en_lvllow; // [63:32]
-    gpio_reg2hw_ctrl_en_input_filter_reg_t ctrl_en_input_filter; // [31:0]
+    gpio_reg2hw_intr_state_reg_t intr_state; // [459:428]
+    gpio_reg2hw_intr_enable_reg_t intr_enable; // [427:396]
+    gpio_reg2hw_intr_test_reg_t intr_test; // [395:363]
+    gpio_reg2hw_direct_out_reg_t direct_out; // [362:330]
+    gpio_reg2hw_masked_out_lower_reg_t masked_out_lower; // [329:296]
+    gpio_reg2hw_masked_out_upper_reg_t masked_out_upper; // [295:262]
+    gpio_reg2hw_direct_oe_reg_t direct_oe; // [261:229]
+    gpio_reg2hw_masked_oe_lower_reg_t masked_oe_lower; // [228:195]
+    gpio_reg2hw_masked_oe_upper_reg_t masked_oe_upper; // [194:161]
+    gpio_reg2hw_intr_ctrl_en_rising_reg_t intr_ctrl_en_rising; // [160:129]
+    gpio_reg2hw_intr_ctrl_en_falling_reg_t intr_ctrl_en_falling; // [128:97]
+    gpio_reg2hw_intr_ctrl_en_lvlhigh_reg_t intr_ctrl_en_lvlhigh; // [96:65]
+    gpio_reg2hw_intr_ctrl_en_lvllow_reg_t intr_ctrl_en_lvllow; // [64:33]
+    gpio_reg2hw_ctrl_en_input_filter_reg_t ctrl_en_input_filter; // [32:1]
   } gpio_reg2hw_t;
 
-  // HW -> register type
+  ///////////////////////////////////////
+  // Internal design logic to register //
+  ///////////////////////////////////////
   typedef struct packed {
-    gpio_hw2reg_intr_state_reg_t intr_state; // [257:225]
-    gpio_hw2reg_data_in_reg_t data_in; // [224:192]
-    gpio_hw2reg_direct_out_reg_t direct_out; // [191:160]
-    gpio_hw2reg_masked_out_lower_reg_t masked_out_lower; // [159:128]
-    gpio_hw2reg_masked_out_upper_reg_t masked_out_upper; // [127:96]
-    gpio_hw2reg_direct_oe_reg_t direct_oe; // [95:64]
-    gpio_hw2reg_masked_oe_lower_reg_t masked_oe_lower; // [63:32]
-    gpio_hw2reg_masked_oe_upper_reg_t masked_oe_upper; // [31:0]
+    gpio_hw2reg_intr_state_reg_t intr_state; // [258:227]
+    gpio_hw2reg_data_in_reg_t data_in; // [226:227]
+    gpio_hw2reg_direct_out_reg_t direct_out; // [226:194]
+    gpio_hw2reg_masked_out_lower_reg_t masked_out_lower; // [193:160]
+    gpio_hw2reg_masked_out_upper_reg_t masked_out_upper; // [159:126]
+    gpio_hw2reg_direct_oe_reg_t direct_oe; // [125:93]
+    gpio_hw2reg_masked_oe_lower_reg_t masked_oe_lower; // [92:59]
+    gpio_hw2reg_masked_oe_upper_reg_t masked_oe_upper; // [58:25]
   } gpio_hw2reg_t;
 
-  // Register offsets
-  parameter logic [BlockAw-1:0] GPIO_INTR_STATE_OFFSET = 6'h 0;
-  parameter logic [BlockAw-1:0] GPIO_INTR_ENABLE_OFFSET = 6'h 4;
-  parameter logic [BlockAw-1:0] GPIO_INTR_TEST_OFFSET = 6'h 8;
-  parameter logic [BlockAw-1:0] GPIO_DATA_IN_OFFSET = 6'h c;
-  parameter logic [BlockAw-1:0] GPIO_DIRECT_OUT_OFFSET = 6'h 10;
-  parameter logic [BlockAw-1:0] GPIO_MASKED_OUT_LOWER_OFFSET = 6'h 14;
-  parameter logic [BlockAw-1:0] GPIO_MASKED_OUT_UPPER_OFFSET = 6'h 18;
-  parameter logic [BlockAw-1:0] GPIO_DIRECT_OE_OFFSET = 6'h 1c;
-  parameter logic [BlockAw-1:0] GPIO_MASKED_OE_LOWER_OFFSET = 6'h 20;
-  parameter logic [BlockAw-1:0] GPIO_MASKED_OE_UPPER_OFFSET = 6'h 24;
-  parameter logic [BlockAw-1:0] GPIO_INTR_CTRL_EN_RISING_OFFSET = 6'h 28;
-  parameter logic [BlockAw-1:0] GPIO_INTR_CTRL_EN_FALLING_OFFSET = 6'h 2c;
-  parameter logic [BlockAw-1:0] GPIO_INTR_CTRL_EN_LVLHIGH_OFFSET = 6'h 30;
-  parameter logic [BlockAw-1:0] GPIO_INTR_CTRL_EN_LVLLOW_OFFSET = 6'h 34;
-  parameter logic [BlockAw-1:0] GPIO_CTRL_EN_INPUT_FILTER_OFFSET = 6'h 38;
+  // Register Address
+  parameter logic [5:0] GPIO_INTR_STATE_OFFSET = 6'h 0;
+  parameter logic [5:0] GPIO_INTR_ENABLE_OFFSET = 6'h 4;
+  parameter logic [5:0] GPIO_INTR_TEST_OFFSET = 6'h 8;
+  parameter logic [5:0] GPIO_DATA_IN_OFFSET = 6'h c;
+  parameter logic [5:0] GPIO_DIRECT_OUT_OFFSET = 6'h 10;
+  parameter logic [5:0] GPIO_MASKED_OUT_LOWER_OFFSET = 6'h 14;
+  parameter logic [5:0] GPIO_MASKED_OUT_UPPER_OFFSET = 6'h 18;
+  parameter logic [5:0] GPIO_DIRECT_OE_OFFSET = 6'h 1c;
+  parameter logic [5:0] GPIO_MASKED_OE_LOWER_OFFSET = 6'h 20;
+  parameter logic [5:0] GPIO_MASKED_OE_UPPER_OFFSET = 6'h 24;
+  parameter logic [5:0] GPIO_INTR_CTRL_EN_RISING_OFFSET = 6'h 28;
+  parameter logic [5:0] GPIO_INTR_CTRL_EN_FALLING_OFFSET = 6'h 2c;
+  parameter logic [5:0] GPIO_INTR_CTRL_EN_LVLHIGH_OFFSET = 6'h 30;
+  parameter logic [5:0] GPIO_INTR_CTRL_EN_LVLLOW_OFFSET = 6'h 34;
+  parameter logic [5:0] GPIO_CTRL_EN_INPUT_FILTER_OFFSET = 6'h 38;
 
-  // Reset values for hwext registers and their fields
-  parameter logic [31:0] GPIO_INTR_TEST_RESVAL = 32'h 0;
-  parameter logic [31:0] GPIO_INTR_TEST_GPIO_RESVAL = 32'h 0;
-  parameter logic [31:0] GPIO_DIRECT_OUT_RESVAL = 32'h 0;
-  parameter logic [31:0] GPIO_MASKED_OUT_LOWER_RESVAL = 32'h 0;
-  parameter logic [31:0] GPIO_MASKED_OUT_UPPER_RESVAL = 32'h 0;
-  parameter logic [31:0] GPIO_DIRECT_OE_RESVAL = 32'h 0;
-  parameter logic [31:0] GPIO_MASKED_OE_LOWER_RESVAL = 32'h 0;
-  parameter logic [31:0] GPIO_MASKED_OE_UPPER_RESVAL = 32'h 0;
 
-  // Register index
+  // Register Index
   typedef enum int {
     GPIO_INTR_STATE,
     GPIO_INTR_ENABLE,
@@ -248,6 +241,5 @@ package gpio_reg_pkg;
     4'b 1111, // index[13] GPIO_INTR_CTRL_EN_LVLLOW
     4'b 1111  // index[14] GPIO_CTRL_EN_INPUT_FILTER
   };
-
 endpackage
 
