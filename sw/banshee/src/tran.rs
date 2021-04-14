10ff89c56665bb743034b189f47b43c5816d78da
Noah Huetter <noahhuetter@gmail.com>
2021-04-14 16:22:36 +0200

    banshee: Mulh/mulhu now correct

--- sw/banshee/src/tran.rs
+++ sw/banshee/src/tran.rs
@@ -2120,22 +2120,34 @@ impl<'a> InstructionTranslator<'a> {
             riscv::OpcodeRdRs1Rs2::Xor => LLVMBuildXor(self.builder, rs1, rs2, name),
             riscv::OpcodeRdRs1Rs2::Mul => LLVMBuildMul(self.builder, rs1, rs2, name),
             riscv::OpcodeRdRs1Rs2::Mulhu => {
-                let tmp = LLVMBuildMul(self.builder, rs1, rs2, name);
-                LLVMBuildLShr(
+                let tmp = LLVMBuildMul(
+                    self.builder,
+                    LLVMBuildZExt(self.builder, rs1, LLVMInt64Type(), NONAME),
+                    LLVMBuildZExt(self.builder, rs2, LLVMInt64Type(), NONAME),
+                    name,
+                );
+                let tmp = LLVMBuildLShr(
                     self.builder,
                     tmp,
-                    LLVMConstInt(LLVMInt32Type(), 32 as u64, 0),
-                    name,
-                )
+                    LLVMConstInt(LLVMInt64Type(), 32 as u64, 0),
+                    NONAME,
+                );
+                LLVMBuildTrunc(self.builder, tmp, LLVMInt32Type(), NONAME)
             }
             riscv::OpcodeRdRs1Rs2::Mulh => {
-                let tmp = LLVMBuildMul(self.builder, rs1, rs2, name);
-                LLVMBuildLShr(
+                let tmp = LLVMBuildMul(
+                    self.builder,
+                    LLVMBuildSExt(self.builder, rs1, LLVMInt64Type(), NONAME),
+                    LLVMBuildSExt(self.builder, rs2, LLVMInt64Type(), NONAME),
+                    name,
+                );
+                let tmp = LLVMBuildLShr(
                     self.builder,
                     tmp,
-                    LLVMConstInt(LLVMInt32Type(), 32 as u64, 0),
-                    name,
-                )
+                    LLVMConstInt(LLVMInt64Type(), 32 as u64, 0),
+                    NONAME,
+                );
+                LLVMBuildTrunc(self.builder, tmp, LLVMInt32Type(), NONAME)
             }
             riscv::OpcodeRdRs1Rs2::Div => LLVMBuildSDiv(self.builder, rs1, rs2, name),
             riscv::OpcodeRdRs1Rs2::Divu => LLVMBuildUDiv(self.builder, rs1, rs2, name),
