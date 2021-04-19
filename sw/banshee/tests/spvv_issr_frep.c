c1f0d90c6e7d4f97b849e2fd57d4daf4a894e295
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2021-04-19 14:26:09 +0200

    banshee: Reformat Rust and C code

--- sw/banshee/tests/spvv_issr_frep.c
+++ sw/banshee/tests/spvv_issr_frep.c
@@ -2,49 +2,50 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include <stdint.h>
-#include "runtime.h"
-#include "math.h"
-
 #include "spvv_issr_frep.h"
 
-// FREP assembly macro
-#define SLA_FREP_ITER(max_inst,max_rep_rnum,stagger_max,stagger_mask,is_outer) \
-    ".word   ("#max_inst     "<< 20) \
-            |("#max_rep_rnum "<< 15) \
-            |("#stagger_max  "<< 12) \
-            |("#stagger_mask "<< 8)  \
-            |("#is_outer     "<< 7)  \
-            |0b0001011\n"
+#include <stdint.h>
 
-#define SLA_CSRCI_FENCE(reg)
+#include "math.h"
+#include "runtime.h"
+
+// FREP assembly macro
+#define SLA_FREP_ITER(max_inst, max_rep_rnum, stagger_max, stagger_mask, \
+                      is_outer)                                          \
+    ".word   (" #max_inst                                                \
+    "<< 20) \
+            |(" #max_rep_rnum                                            \
+    "<< 15) \
+            |(" #stagger_max                                             \
+    "<< 12) \
+            |(" #stagger_mask                                            \
+    "<< 8)  \
+            |(" #is_outer                                                \
+    "<< 7)  \
+             |0b0001011\n"
 
 // Naive compiled SpVV kernel
-static inline void svec16_dvec_dotp_naive(
-    double *    const   vals_a,
-    uint16_t *  const   idcs_a,
-    uint32_t    const   len_a,
-    double *    const   vals_b,
-    double *    const   res
-) {
+static inline void svec16_dvec_dotp_naive(double* const vals_a,
+                                          uint16_t* const idcs_a,
+                                          uint32_t const len_a,
+                                          double* const vals_b,
+                                          double* const res) {
     double acc = 0.0;
     for (int i = 0; i < len_a; ++i) {
-        acc += vals_a[i]*vals_b[idcs_a[i]];
+        acc += vals_a[i] * vals_b[idcs_a[i]];
     }
     *res = acc;
 }
 
 // Optimized assembly kernel using ISSR + SSR
-static inline void svec16_dvec_dotp_opt_issr(
-    double *            vals_a,
-    uint16_t *          idcs_a,
-    uint32_t    const   len_a,
-    double *    const   vals_b,
-    double *    const   res
-) {
+static inline void svec16_dvec_dotp_opt_issr(double* vals_a, uint16_t* idcs_a,
+                                             uint32_t const len_a,
+                                             double* const vals_b,
+                                             double* const res) {
     if (len_a == 0) return;
     asm volatile (
         // SSR setup
+        // TODO: Use SSR extensions, shared config write here once available
         "fmv.d      ft2, %[f0]          \n"     // Pull ahead to prevent bubble
         "li         t0, 8               \n"
         "li         t1, 1               \n"
@@ -55,11 +56,7 @@ static inline void svec16_dvec_dotp_opt_issr(
         "sw         %3, 344(%5)         \n"     // idx_base[1]
         "sw         t0, 48 (%5)         \n"     // stride_0[0]
         "sw         t0, 304(%5)         \n"     // stride_1[0]
-
-
         "sw         %1, 384(%5)         \n"     // rptr_indir[1]
-        //"sw         %1, 448(%5)         \n"     // rptr_0[1]
-
         "sw         %0, 192(%5)         \n"     // rptr_0[0]
         // Enable SSRs
         "csrsi      0x7C0, 1            \n"
@@ -94,8 +91,8 @@ int main() {
     double res_issr, res_gcc;
     double const EPS = 1e-6;
     // Run kernels
-    svec16_dvec_dotp_naive(a_vals, a_idcs, A_DATA_LEN,b_vals, &res_gcc);
-    svec16_dvec_dotp_opt_issr(a_vals, a_idcs, A_DATA_LEN,b_vals, &res_issr);
+    svec16_dvec_dotp_naive(a_vals, a_idcs, A_DATA_LEN, b_vals, &res_gcc);
+    svec16_dvec_dotp_opt_issr(a_vals, a_idcs, A_DATA_LEN, b_vals, &res_issr);
     // Check results
     return (fabs(res_gcc - gold) > EPS || fabs(res_issr - gold) > EPS);
 }
