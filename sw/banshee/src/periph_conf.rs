82eaace8792ba7c90c601817193c19284150d19a
Julien Eudine <julien@eudine.fr>
2021-07-05 16:19:26 +0200

    WIP

--- sw/banshee/src/periph_conf.rs
+++ sw/banshee/src/periph_conf.rs
@@ -3,7 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::peripherals::Peripheral;
-use std::sync::atomic::AtomicPtr;
+use std::sync::atomic::{AtomicU32, AtomicU8, Ordering};
+use PeriphCounterState::{CountLoad, CountLoadStore, CountStore, Disabled};
 
 pub fn create_peripherals() -> Vec<Box<dyn Peripheral>> {
     vec![Box::new(PeriphCounter::default())]
@@ -11,8 +12,8 @@ pub fn create_peripherals() -> Vec<Box<dyn Peripheral>> {
 
 #[derive(Default)]
 struct PeriphCounter {
-    state: AtomicPtr<PeriphCounterState>,
-    counter: u32,
+    state: AtomicU8,
+    counter: AtomicU32,
 }
 
 impl Peripheral for PeriphCounter {
@@ -21,31 +22,42 @@ impl Peripheral for PeriphCounter {
     }
 
     fn store(&self, addr: u32, val: u32, size: u8) {
-        /*
-        let val = val && 0x3;
+        let val = val & 0x3;
         if val == 0 {
-            sel
+            self.state.store(Disabled as u8, Ordering::SeqCst);
+            self.counter.store(0, Ordering::SeqCst);
+        } else if val == 1 {
+            self.state.store(CountLoad as u8, Ordering::SeqCst);
+            self.counter.store(0, Ordering::SeqCst);
+        } else if val == 2 {
+            self.state.store(CountStore as u8, Ordering::SeqCst);
+            self.counter.store(0, Ordering::SeqCst);
+        } else if val == 3 {
+            self.state.store(CountLoadStore as u8, Ordering::SeqCst);
+            self.counter.store(0, Ordering::SeqCst);
+        } else if (self.state.load(Ordering::SeqCst) & 0x2) == 0x2 {
+            self.counter.fetch_add(1, Ordering::SeqCst);
         }
-        */
-        println!("test");
     }
 
-    fn load(&self, addr: u32, size: u8) -> u32 {
-        0
+    fn load(&self, addr: u32, _: u8) -> u32 {
+        let mut res: u32 = 0;
+        if addr > 3 {
+            res = self.counter.load(Ordering::SeqCst) >> 8 * (addr - 4);
+        } else if addr == 0 {
+            res = self.state.load(Ordering::SeqCst) as u32;
+        }
+        let state = self.state.load(Ordering::SeqCst);
+        if (state == CountLoad as u8 || state == CountLoadStore as u8) {
+            self.counter.fetch_add(1, Ordering::SeqCst);
+        }
+        res
     }
 }
-
-struct AtomicPeriphCounterState {}
 
 enum PeriphCounterState {
-    Disabled,
-    CountLoad,
-    CountStore,
-    CountLoadStore,
-}
-
-impl Default for PeriphCounterState {
-    fn default() -> Self {
-        PeriphCounterState::Disabled
-    }
+    Disabled = 0,
+    CountLoad = 1,
+    CountStore = 2,
+    CountLoadStore = 3,
 }
