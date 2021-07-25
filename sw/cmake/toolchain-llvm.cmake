ff2e6043cbb0621f7d9565d9123f0576231e66ec
Noah Huetter <noahhuetter@gmail.com>
2021-07-25 14:31:47 +0200

    sw: Add toolchain-llvm.cmake

---
+++ sw/cmake/toolchain-llvm.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_C_COMPILER riscv32-unknown-elf-clang)
+set(CMAKE_CXX_COMPILER riscv32-unknown-elf-clang++)
+set(CMAKE_OBJCOPY llvm-objcopy)
+set(CMAKE_OBJDUMP llvm-objdump)
+set(CMAKE_AR llvm-ar)
+set(CMAKE_STRIP llvm-strip)
+set(CMAKE_RANLIB llvm-ranlib)
+
+# LTO
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION false)
+
+# -march=rv32imafd -mabi=ilp32d
+add_compile_options(-mcpu=snitch -mcmodel=medany -ffast-math -fno-builtin-printf -fno-common)
+add_link_options(-mcpu=snitch -nostartfiles -fuse-ld=lld -Wl,--image-base=0x80000000)
+# add_link_options(-Wl,--verbose)
+
+link_libraries(-lm)
+
+add_compile_options(-ffunction-sections)
+add_compile_options(-Wextra)
