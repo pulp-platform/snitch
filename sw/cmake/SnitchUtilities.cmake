cdcb0771dec3eeb9806017b7efa6bf362509aeb5
Noah Huetter <noahhuetter@gmail.com>
2021-07-26 16:00:38 +0200

    sw: Explicit linker script

--- sw/cmake/SnitchUtilities.cmake
+++ sw/cmake/SnitchUtilities.cmake
@@ -15,6 +15,11 @@ if (SNITCH_SIMULATOR)
 endif()
 message(STATUS "Using runtime: ${SNITCH_RUNTIME}")
 
+
+# Linker script
+set(LINKER_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/../snRuntime/link/banshee.ld" CACHE FILEPATH "Linker Script")
+message(STATUS "Using linker script: ${LINKER_SCRIPT}")
+
 # Select to build the tests
 set(BUILD_TESTS OFF CACHE BOOL "Build test executables")
 
@@ -29,6 +34,7 @@ endmacro()
 macro(add_snitch_executable name)
     add_executable(${ARGV})
     target_link_libraries(${name} ${SNITCH_RUNTIME})
+    target_link_options(${name} PRIVATE -T ${LINKER_SCRIPT})
     add_custom_command(
         TARGET ${name}
         POST_BUILD
