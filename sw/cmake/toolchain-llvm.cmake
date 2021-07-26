252d9c74f6841f8ab8c37dab37878da2d44d4ca7
Noah Huetter <noahhuetter@gmail.com>
2021-07-26 15:52:17 +0200

    sw: Snitch cpu for objdump

--- sw/cmake/toolchain-llvm.cmake
+++ sw/cmake/toolchain-llvm.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_C_COMPILER riscv32-unknown-elf-clang)
 set(CMAKE_CXX_COMPILER riscv32-unknown-elf-clang++)
 set(CMAKE_OBJCOPY llvm-objcopy)
-set(CMAKE_OBJDUMP llvm-objdump)
+set(CMAKE_OBJDUMP llvm-objdump --mcpu=snitch)
 set(CMAKE_AR llvm-ar)
 set(CMAKE_STRIP llvm-strip)
 set(CMAKE_RANLIB llvm-ranlib)
