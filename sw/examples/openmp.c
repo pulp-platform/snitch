72dc9b1ee5c8451def5564d96d5df175f7f2961e
Noah Huetter <noahhuetter@gmail.com>
2021-07-30 15:17:58 +0200

    lint: C license header

--- sw/examples/openmp.c
+++ sw/examples/openmp.c
@@ -1,4 +1,6 @@
-
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
 
 int main(int argc, char** args) {
 	return 0;
