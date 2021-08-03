d527fd6f2a544133d8d95b62fd8f47186e6926da
Noah Huetter <noahhuetter@gmail.com>
2021-08-03 09:32:08 +0200

    sw: Lint

--- sw/snRuntime/src/team.c
+++ sw/snRuntime/src/team.c
@@ -6,7 +6,8 @@
 #include "snrt.h"
 
 __thread struct snrt_team *_snrt_team_current;
-const uint32_t _snrt_team_size __attribute__((section(".rodata"))) = sizeof(struct snrt_team_root);
+const uint32_t _snrt_team_size __attribute__((section(".rodata"))) =
+    sizeof(struct snrt_team_root);
 
 uint32_t __attribute__((pure)) snrt_hartid() {
     uint32_t hartid;
