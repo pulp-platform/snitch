6d64bc838801b9c93bc8a89b1d04b52c74344055
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2021-08-06 10:13:05 +0200

    Fix license header in `spm_1p_adv`

--- hw/ip/spm_interface/src/spm_1p_adv.sv
+++ hw/ip/spm_interface/src/spm_1p_adv.sv
@@ -1,5 +1,4 @@
 // Copyright 2021 ETH Zurich and University of Bologna.
-// Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
