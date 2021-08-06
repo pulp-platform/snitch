ddd5c88e9736bb23960864864baf9b1948144c70
Gianna Paulin <pauling@student.ethz.ch>
2021-08-06 14:03:29 +0200

    add seperate crate for FFI bindings of C Flexfloat library

---
+++ sw/banshee/flexfloat/src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+
+include!("./bindings.rs");
+
+/// SOFTFLOAT FORMATS
+pub const FP16EXP: u8 = 5;
+pub const FP16MAN: u8 = 10;
