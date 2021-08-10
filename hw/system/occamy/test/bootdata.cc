e28929b6220bcc8c6e3f3ec826b6f0c3b7a86511
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-08-10 16:53:17 +0200

    occamy: Add correct auto-generated

--- hw/system/occamy/test/bootdata.cc
+++ hw/system/occamy/test/bootdata.cc
@@ -6,16 +6,15 @@
 
 namespace sim {
 
-// TODO(zarubaf): Auto-generate
 const BootData BOOTDATA = {
     .boot_addr = 0x1000000,
     .core_count = 9,
     .hartid_base = 1,
     .tcdm_start = 0x10000000,
-    .tcdm_size = 0x00020000,
-    .tcdm_offset = 0x00040000,
+    .tcdm_size = 0x20000,
+    .tcdm_offset = 0x40000,
     .global_mem_start = 0x90000000,
-    .global_mem_end = 0xC0000000,
+    .global_mem_end = 0x100000000,
     .cluster_count = 4,
     .s1_quadrant_count = 8
 };
