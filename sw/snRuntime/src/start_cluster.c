1108aa7038b6f4d1cf0106eb5426f8a55b55945c
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-08-11 12:55:32 +0200

    runtime: Merge cluster and occamy runtime

--- sw/snRuntime/src/start_cluster.c
+++ sw/snRuntime/src/start_cluster.c
@@ -20,11 +20,16 @@ struct snrt_cluster_bootdata {
     uint32_t core_count;
     uint32_t hartid_base;
     uint32_t tcdm_start;
-    uint32_t tcdm_end;
+    uint32_t tcdm_size;
+    uint32_t tcdm_offset;
+    uint64_t global_mem_start;
+    uint64_t global_mem_end;
+    uint32_t cluster_count;
+    uint32_t s1_quadrant_count;
 };
 
 // Rudimentary string buffer for putc calls.
-extern uint32_t _edram;
+extern uint32_t _end;
 #define PUTC_BUFFER_LEN (1024 - sizeof(size_t))
 struct putc_buffer_header {
     size_t size;
@@ -33,7 +38,7 @@ struct putc_buffer_header {
 static volatile struct putc_buffer {
     struct putc_buffer_header hdr;
     char data[PUTC_BUFFER_LEN];
-} *const putc_buffer = (void *)&_edram;
+} *const putc_buffer = (void *)&_end;
 
 void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
                      void *spm_start, void *spm_end,
@@ -42,26 +47,36 @@ void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
     team->base.root = team;
     team->device_tree = (void *)bootdata;
     team->global_core_base_hartid = bootdata->hartid_base;
-    team->global_core_num = bootdata->core_count;
-    team->cluster_idx = 0;
-    team->cluster_num = 1;
+    team->global_core_num = bootdata->core_count *
+        bootdata->cluster_count * bootdata->s1_quadrant_count;
+    team->cluster_idx =
+        (snrt_hartid() - bootdata->hartid_base) / bootdata->core_count;
+    team->cluster_num = bootdata->cluster_count;
     team->cluster_core_base_hartid = bootdata->hartid_base;
-    team->cluster_core_num = bootdata->core_count;
-    team->global_mem.start =
-        (void *)0x90000000;  // TODO: Read this from bootdata
-    team->global_mem.end =
-        (void *)0x100000000;  // TODO: Read this from bootdata
+    team->cluster_core_num = cluster_core_num;
+    team->global_mem.start = bootdata->global_mem_start;
+    team->global_mem.end = bootdata->global_mem_end;
     team->cluster_mem.start = spm_start;
     team->cluster_mem.end = spm_end;
-    team->barrier_reg_ptr = spm_end + 0x30;
+    team->barrier_reg_ptr = spm_start + bootdata->tcdm_size + 0x30;
+
+    // Initialize cluster barrier
+    team->cluster_barrier.barrier = 0;
+    team->cluster_barrier.barrier_iteration = 0;
+
+    // Allocate and initialize global barrier
+    team->global_barrier = (void*)team->global_mem.start;
+    team->global_barrier->barrier = 0;
+    team->global_barrier->barrier_iteration = 0;
+    team->global_mem.start += sizeof(struct snrt_barrier);
 
     // Allocate memory for a global mailbox.
-    team->global_mailbox = team->global_mem.start;
+    team->global_mailbox = (void*)team->global_mem.start;
     team->global_mem.start += sizeof(struct snrt_mailbox);
 
     // Allocate memory for a cluster mailbox.
     team->cluster_mem.end -= sizeof(struct snrt_mailbox);
-    team->cluster_mailbox = team->cluster_mem.end;
+    team->cluster_mailbox = (void*)team->cluster_mem.end;
 
     _snrt_team_current = &team->base;
 
