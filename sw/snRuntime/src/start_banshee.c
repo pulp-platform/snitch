e0ea03d6f66bb2dfd7e06f5379fa1e2af8fe79f5
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-08-11 14:46:31 +0200

    runtime: Revert banshee dram changes

--- sw/snRuntime/src/start_banshee.c
+++ sw/snRuntime/src/start_banshee.c
@@ -8,7 +8,8 @@ extern const uint32_t _snrt_banshee_cluster_core_num;
 extern const uint32_t _snrt_banshee_cluster_base_hartid;
 extern const uint32_t _snrt_banshee_cluster_num;
 extern const uint32_t _snrt_banshee_cluster_id;
-uint64_t const _snrt_banshee_global_offset = (uint64_t)0x10000000;
+uint64_t const _snrt_banshee_global_start = (uint64_t)0x90000000;
+uint64_t const _snrt_banshee_global_end = (uint64_t)0x100000000;
 
 const uint32_t snrt_stack_size __attribute__((weak, section(".rodata"))) = 10;
 
@@ -25,7 +26,7 @@ void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
     team->cluster_num = _snrt_banshee_cluster_num;
     team->cluster_core_base_hartid = _snrt_banshee_cluster_base_hartid;
     team->cluster_core_num = _snrt_banshee_cluster_core_num;
-    team->global_mem.start = _snrt_banshee_global_start + _snrt_banshee_global_offset;
+    team->global_mem.start = _snrt_banshee_global_start;
     team->global_mem.end = _snrt_banshee_global_end;
     team->cluster_mem.start = spm_start;
     team->cluster_mem.end = spm_end;
