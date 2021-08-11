4e02b5676a78e389774f5fd0bd6edff2e8786872
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-08-11 14:21:49 +0200

    config: Expose entire dram

--- sw/snRuntime/src/start_banshee.c
+++ sw/snRuntime/src/start_banshee.c
@@ -8,8 +8,7 @@ extern const uint32_t _snrt_banshee_cluster_core_num;
 extern const uint32_t _snrt_banshee_cluster_base_hartid;
 extern const uint32_t _snrt_banshee_cluster_num;
 extern const uint32_t _snrt_banshee_cluster_id;
-uint64_t const _snrt_banshee_global_start = (uint64_t)0x90000000;
-uint64_t const _snrt_banshee_global_end = (uint64_t)0x100000000;
+uint64_t const _snrt_banshee_global_offset = (uint64_t)0x10000000;
 
 const uint32_t snrt_stack_size __attribute__((weak, section(".rodata"))) = 10;
 
@@ -26,14 +25,12 @@ void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
     team->cluster_num = _snrt_banshee_cluster_num;
     team->cluster_core_base_hartid = _snrt_banshee_cluster_base_hartid;
     team->cluster_core_num = _snrt_banshee_cluster_core_num;
-    team->global_mem.start = _snrt_banshee_global_start;
+    team->global_mem.start = _snrt_banshee_global_start + _snrt_banshee_global_offset;
     team->global_mem.end = _snrt_banshee_global_end;
     team->cluster_mem.start = spm_start;
     team->cluster_mem.end = spm_end;
 
     // Allocate and initialize global barrier
-    // TODO: Atomics to global memory are currently
-    // not supported by banshee
     team->global_barrier = (void*)team->global_mem.start;
     team->global_barrier->barrier = 0;
     team->global_barrier->barrier_iteration = 0;
--- sw/snRuntime/src/start_cluster.c
+++ sw/snRuntime/src/start_cluster.c
@@ -8,8 +8,7 @@ extern const uint32_t _snrt_cluster_cluster_core_num;
 extern const uint32_t _snrt_cluster_cluster_base_hartid;
 extern const uint32_t _snrt_cluster_cluster_num;
 extern const uint32_t _snrt_cluster_cluster_id;
-void *const _snrt_cluster_global_start = (void *)0x90000000;
-void *const _snrt_cluster_global_end = (void *)0x100000000;
+void *const _snrt_cluster_global_offset = (void *)0x10000000;
 
 const uint32_t snrt_stack_size __attribute__((weak, section(".rodata"))) = 10;
 
@@ -54,7 +53,7 @@ void _snrt_init_team(uint32_t cluster_core_id, uint32_t cluster_core_num,
     team->cluster_num = bootdata->cluster_count;
     team->cluster_core_base_hartid = bootdata->hartid_base;
     team->cluster_core_num = cluster_core_num;
-    team->global_mem.start = bootdata->global_mem_start;
+    team->global_mem.start = bootdata->global_mem_start + _snrt_cluster_global_offset;
     team->global_mem.end = bootdata->global_mem_end;
     team->cluster_mem.start = spm_start;
     team->cluster_mem.end = spm_end;
