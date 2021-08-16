3863f037f424500fb799991560e15fc9cbf8454f
Noah Huetter <noahhuetter@gmail.com>
2021-08-16 16:20:29 +0200

    sw: Cleanup interrupt test

--- sw/snRuntime/tests/interrupt.c
+++ sw/snRuntime/tests/interrupt.c
@@ -8,18 +8,12 @@
 
 int reboot = 0;
 
-// #define tprintf(f_, ...) printf((f_), __VA_ARGS__)
-#define tprintf(f_, ...) while (0)
+#define tprintf(f_, ...) printf((f_), __VA_ARGS__)
+// #define tprintf(f_, ...) while (0
 
 int main() {
     unsigned core_idx = snrt_global_core_idx();
     unsigned core_num = snrt_global_core_num();
-
-    // self trigger interrupt through CSR
-    snrt_interrupt_enable(IRQ_M_SOFT);
-    snrt_interrupt_global_enable();
-    set_csr(mip, 1 << IRQ_M_SOFT);
-    return 0;
 
     volatile int32_t *INTERRUPT_FLAG = ((int32_t *)snrt_cluster_memory().start);
 
@@ -27,12 +21,12 @@ int main() {
     // polls the flag and checks for correctness
     if (core_idx == 0) {
         for (int i = 1; i < core_num; i++) {
-            tprintf("trig %d .. \n", i);
+            tprintf("IRQ %d ..", i);
             *INTERRUPT_FLAG = -1;
             snrt_int_sw_set(i);
             while (*INTERRUPT_FLAG != i)
                 ;
-            tprintf("  OK\n", *INTERRUPT_FLAG);
+            tprintf("OK\n", *INTERRUPT_FLAG);
         }
     } else {
         snrt_interrupt_enable(IRQ_M_SOFT);
@@ -64,7 +58,7 @@ int main() {
 }
 
 void irq_m_soft(uint32_t hartid) {
-    // snrt_int_sw_clear(hartid);
-    // int32_t *INTERRUPT_FLAG = ((int32_t *)snrt_cluster_memory().start);
-    // *INTERRUPT_FLAG = hartid;
+    snrt_int_sw_clear(hartid);
+    int32_t *INTERRUPT_FLAG = ((int32_t *)snrt_cluster_memory().start);
+    *INTERRUPT_FLAG = hartid;
 }
