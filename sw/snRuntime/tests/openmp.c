4dfab16ac85136c166e5663b6262791a246dbdcb
Noah Huetter <noahhuetter@gmail.com>
2021-09-09 17:02:23 +0200

    omp: Faster AXPY test and wave file

--- sw/snRuntime/tests/openmp.c
+++ sw/snRuntime/tests/openmp.c
@@ -9,6 +9,7 @@
 #include "snrt.h"
 
 #define AXPY_N 64
+#define NUMTHREADS 8
 
 // #define tprintf(...) printf(__VA_ARGS__)
 #define tprintf(...) while (0)
@@ -16,7 +17,10 @@
 volatile static uint32_t sum = 0;
 
 unsigned __attribute__((noinline)) static_schedule(void) {
-    static double data_x[AXPY_N], data_y[AXPY_N], data_a;
+    static double *data_x, *data_y, data_a;
+
+    data_x = snrt_l1alloc(sizeof(double) * AXPY_N);
+    data_y = snrt_l1alloc(sizeof(double) * AXPY_N);
 
     // Init data
     data_a = 10.0;
@@ -26,17 +30,21 @@ unsigned __attribute__((noinline)) static_schedule(void) {
     }
 
     // compute
-#pragma omp parallel
+#pragma omp parallel firstprivate(data_a, data_x, data_y)
     {
-        __builtin_ssr_setup_1d_r(0, 0, AXPY_N - 1, sizeof(double),
+        // DM, rep, bound, stride, data
+        __builtin_ssr_setup_1d_r(0, 0, AXPY_N / NUMTHREADS - 1, sizeof(double),
                                  &data_x[AXPY_N / 8 * omp_get_thread_num()]);
-        __builtin_ssr_setup_1d_r(1, 0, AXPY_N - 1, sizeof(double),
+        __builtin_ssr_setup_1d_r(1, 0, AXPY_N / NUMTHREADS - 1, sizeof(double),
+                                 &data_y[AXPY_N / 8 * omp_get_thread_num()]);
+        __builtin_ssr_setup_1d_w(2, 0, AXPY_N / NUMTHREADS - 1, sizeof(double),
                                  &data_y[AXPY_N / 8 * omp_get_thread_num()]);
         __builtin_ssr_enable();
 #pragma omp for schedule(static)
         for (unsigned i = 0; i < AXPY_N; i++) {
             // data_y[i] = data_a * data_x[i] + data_y[i];
-            data_y[i] = data_a * __builtin_ssr_pop(0) + __builtin_ssr_pop(1);
+            __builtin_ssr_push(
+                2, data_a * __builtin_ssr_pop(0) + __builtin_ssr_pop(1));
         }
         __builtin_ssr_disable();
     }
@@ -74,8 +82,8 @@ int main() {
     __snrt_omp_bootstrap(core_idx);
 
     // Static schedule test
-    // err |= static_schedule() << 0;
-    // OMP_PROF(omp_print_prof());
+    err |= static_schedule() << 0;
+    OMP_PROF(omp_print_prof());
     // Launch overhead test
     err |= paralell_section() << 1;
     OMP_PROF(omp_print_prof());
