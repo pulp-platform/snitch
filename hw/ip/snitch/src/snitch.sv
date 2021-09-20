a6ce709819848a2c1ba72c1a7cf4c478fce84236
Gianna Paulin <pauling@student.ethz.ch>
2021-09-20 14:45:00 +0200

    PR #256 resolve first comment of stmach

--- hw/ip/snitch/src/snitch.sv
+++ hw/ip/snitch/src/snitch.sv
@@ -2381,7 +2381,7 @@ module snitch import snitch_pkg::*; import riscv_instr::*; #(
           CSR_FMODE: begin
             if (FP_EN) begin
               csr_rvalue = {30'b0, fcsr_q.fmode};
-              if (!exception) fcsr_d.fmode = fpnew_pkg::fmt_mode_t'(opa[1:0]);
+              if (!exception) fcsr_d.fmode = fpnew_pkg::fmt_mode_t'(alu_result[1:0]);
             end else illegal_csr = 1'b1;
           end
           default: csr_rvalue = '0;
