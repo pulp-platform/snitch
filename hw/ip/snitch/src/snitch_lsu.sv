d560e1598fa7e34b760c10d32d9e2daf377d8cce
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-10-04 12:02:47 +0200

    Enable NaN-boxing for FP16 and FP8

--- hw/ip/snitch/src/snitch_lsu.sv
+++ hw/ip/snitch/src/snitch_lsu.sv
@@ -162,8 +162,8 @@ module snitch_lsu #(
   assign shifted_data = data_rsp_i.p.data >> {laq_out.offset, 3'b000};
   always_comb begin
     unique case (laq_out.size)
-      2'b00: ld_result = {{56{shifted_data[7] & laq_out.sign_ext}}, shifted_data[7:0]};
-      2'b01: ld_result = {{48{shifted_data[15] & laq_out.sign_ext}}, shifted_data[15:0]};
+      2'b00: ld_result = {{56{(shifted_data[7] | NaNBox) & laq_out.sign_ext}}, shifted_data[7:0]};
+      2'b01: ld_result = {{48{(shifted_data[15] | NaNBox) & laq_out.sign_ext}}, shifted_data[15:0]};
       2'b10: ld_result = {{32{(shifted_data[31] | NaNBox) & laq_out.sign_ext}}, shifted_data[31:0]};
       2'b11: ld_result = shifted_data;
       default: ld_result = shifted_data;
