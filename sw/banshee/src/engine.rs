ac033c2cd611573037392b459c189d7f80725dc0
Tim Fischer <fischeti@iis.ee.ethz.ch>
2022-02-16 22:37:29 +0100

    banshee: Emit float register values as float instead of hex (#340)
    * banshee float format
    
    * fix formatting
    
    * fix alignment
    
    * SIMD formatting
    
    * lint: Banshee sources

--- sw/banshee/src/engine.rs
+++ sw/banshee/src/engine.rs
@@ -968,8 +968,26 @@ impl<'a, 'b> Cpu<'a, 'b> {
             TraceAccess::RMWMem => format!("AMO:{:08x}", data as u32),
             TraceAccess::ReadReg(x) => format!("x{}:{:08x}", x, data as u32),
             TraceAccess::WriteReg(x) => format!("x{}={:08x}", x, data as u32),
-            TraceAccess::ReadFReg(x) => format!("f{}:{:016x}", x, data),
-            TraceAccess::WriteFReg(x) => format!("f{}={:016x}", x, data),
+            TraceAccess::ReadFReg(x) => format!("f{:02}:{:>16.6}", x, f64::from_bits(data)),
+            TraceAccess::WriteFReg(x) => format!("f{:02}={:>16.6}", x, f64::from_bits(data)),
+            TraceAccess::ReadF32Reg(x) => {
+                format!("f{:02}:{:>12.4}", x, f32::from_bits(data as u32))
+            }
+            TraceAccess::WriteF32Reg(x) => {
+                format!("f{:02}={:>12.4}", x, f32::from_bits(data as u32))
+            }
+            TraceAccess::Readvf64sReg(x) => format!(
+                "f{:02}:[{:>12.4}, {:>12.4}]",
+                x,
+                f32::from_bits((data >> 32) as u32),
+                f32::from_bits((data) as u32)
+            ),
+            TraceAccess::Writevf64sReg(x) => format!(
+                "f{:02}=[{:>12.4}, {:>12.4}]",
+                x,
+                f32::from_bits((data >> 32) as u32),
+                f32::from_bits((data) as u32)
+            ),
         });
         let args = args.join(" ");
 
@@ -1228,9 +1246,13 @@ pub enum TraceAccess {
     ReadMem,
     ReadReg(u8),
     ReadFReg(u8),
+    ReadF32Reg(u8),
+    Readvf64sReg(u8),
     WriteMem,
     WriteReg(u8),
     WriteFReg(u8),
+    WriteF32Reg(u8),
+    Writevf64sReg(u8),
     RMWMem,
 }
 
