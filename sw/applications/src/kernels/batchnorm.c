2203ff10e35050e3566e32802b0690d4a1b061e9
Tim Fischer <fischeti@iis.ee.ethz.ch>
2022-02-17 13:50:11 +0100

    Remove duplicate docstrings

--- sw/applications/src/kernels/batchnorm.c
+++ sw/applications/src/kernels/batchnorm.c
@@ -4,19 +4,6 @@
 
 #include "snrt.h"
 
-/**
- * @brief implementation of a FP64 batchnorm as a linear combination
- * y = gamma * x + beta
- *
- * @param ifmap pointer to input feature map
- * @param gamma pointer to gamma
- * @param beta pointer to beta
- * @param ofmap pointer to output feature map
- * @param OW width of output feature map
- * @param CI number of input channels
- * @param compute_num number of compute units
- * @param setup_SSR setup SSR strides and bounds
- */
 void batchnorm_fp64(double *ifmap, double *gamma, double *beta, double *ofmap,
                     uint32_t OW, uint32_t CI, uint32_t compute_num,
                     uint32_t setup_SSR) {
--- sw/applications/src/kernels/maxpool.c
+++ sw/applications/src/kernels/maxpool.c
@@ -4,16 +4,6 @@
 
 #include "snrt.h"
 
-/**
- * @brief implementation of FP64 maxpooling
- *
- * @param ifmap pointer to input feature map
- * @param ofmap pointer to output feature map
- * @param CI number of input channels
- * @param FH height of filter
- * @param FW width of filter
- * @param compute_num number of compute units
- */
 void maxpool_fp64(double *ifmap, double *ofmap, uint32_t CI, uint32_t FH,
                   uint32_t FW, uint32_t compute_num) {
     for (uint32_t ci = 0; ci < CI; ci += compute_num) {
