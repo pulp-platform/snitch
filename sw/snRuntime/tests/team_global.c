4b7f13e05e8eb363d042060f05e7b72f2184a8db
Gianna Paulin <pauling@student.ethz.ch>
2022-02-23 21:20:38 +0100

    snRuntime-tests: update team_global test to snitch_cluster configuration

--- sw/snRuntime/tests/team_global.c
+++ sw/snRuntime/tests/team_global.c
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// Must run with `--base-hartid=3 --num-cores=4 --num-clusters=2`
+// Must run with `--base-hartid=0 --num-cores=9 --num-clusters=1`
 #include <snrt.h>
 
 static const struct {
@@ -11,13 +11,13 @@ static const struct {
 } table;
 
 int main() {
-    uint32_t i = snrt_hartid() - 3;  // shift by base hartid
+    uint32_t i = snrt_hartid() - 0;  // shift by base hartid
     uint32_t errors = 0;
     errors += (snrt_global_core_idx() != i);
-    errors += (snrt_global_core_num() != 8);
-    errors += (snrt_cluster_idx() != i / 4);
-    errors += (snrt_cluster_num() != 2);
-    errors += (snrt_cluster_core_idx() != i % 4);
-    errors += (snrt_cluster_core_num() != 4);
+    errors += (snrt_global_core_num() != 9);
+    errors += (snrt_cluster_idx() != i / 1);
+    errors += (snrt_cluster_num() != 1);
+    errors += (snrt_cluster_core_idx() != i % 9);
+    errors += (snrt_cluster_core_num() != 9);
     return errors;
 }
