9fcf671b136ca649ec601de83c0e12786daa0d7f
Noah Huetter <noahhuetter@gmail.com>
2022-03-02 15:38:03 +0100

    new synth/impl flow

--- hw/system/occamy/fpga/occamy_vcu128.tcl
+++ hw/system/occamy/fpga/occamy_vcu128.tcl
@@ -20,7 +20,7 @@ create_project $project ./$project -force -part xcvu37p-fsvh2892-2L-e
 set_property board_part xilinx.com:vcu128:part0:1.0 [current_project]
 set_property XPM_LIBRARIES XPM_MEMORY [current_project]
 
-set_property ip_repo_paths ./vivado_ips [current_project]
+set_property ip_repo_paths ./vivado_ips/build [current_project]
 update_ip_catalog
 
 # Create block design
@@ -42,19 +42,51 @@ export_ip_user_files -of_objects [get_files ./occamy_vcu128/occamy_vcu128.srcs/s
 create_ip_run [get_files -of_objects [get_fileset sources_1] ./occamy_vcu128/occamy_vcu128.srcs/sources_1/bd/occamy_vcu128/occamy_vcu128.bd]
 
 # Re-add occamy includes
-export_ip_user_files -of_objects [get_ips occamy_vcu128_occamy_xilinx_0_0] -no_script -sync -force -quiet
-eval [exec sed {s/current_fileset/get_filesets occamy_vcu128_occamy_xilinx_0_0/} define_defines_includes_no_simset.tcl]
+set build occamy_vcu128
+source fix_includes.tcl
 
 # Do NOT insert BUFGs on high-fanout nets (e.g. reset). This will backfire during placement.
 set_param logicopt.enableBUFGinsertHFN no
 
-# Synthesize
-foreach run [list synth_1 occamy_vcu128_occamy_xilinx_0_0_synth_1] {
- set_property strategy Flow_AlternateRoutability [get_runs $run]
- set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING true [get_runs $run]
+
+# OOC synthesis of changed IP
+set synth_runs [get_runs *synth*]
+set synth_1_idx [lsearch $synth_runs "synth_1"]
+set all_ooc_synth [lreplace $synth_runs $synth_1_idx $synth_1_idx]
+set runs_queued {}
+foreach run $all_ooc_synth {
+    if {[get_property PROGRESS [get_run $run]] != "100%"} {
+        puts "Launching run $run"
+        lappend runs_queued $run
+        # Default synthesis strategy
+        set_property strategy Flow_AlternateRoutability [get_runs $run]
+    } else {
+        puts "Skipping 100% complete run: $run"
+    }
 }
-launch_runs synth_1 -jobs 12
-wait_on_run synth_1
+if {[llength $runs_queued] != 0} {
+    reset_run $runs_queued
+    launch_runs $runs_queued -jobs ${nproc}
+    puts "Waiting on $runs_queued"
+    foreach run $runs_queued {
+        wait_on_run $run
+    }
+    # reset main synthesis
+    reset_run synth_1
+}
+
+# top-level synthesis
+set run synth_1
+if {[get_property PROGRESS [get_run $run]] != "100%"} {
+    puts "Launching run $run"
+    reset_run $run
+    set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING false [get_runs $run]
+    launch_runs $run -jobs ${nproc}
+    wait_on_run $run
+} else {
+    puts "Skipping 100% complete run: $run"
+}
+
 
 # Create ILA. Attach all signals that were previously marked debug.
 # For occamy-internal signals: Add "(* mark_debug = "true" *)" before signal definition in HDL code.
@@ -112,7 +144,7 @@ if ($DEBUG) {
 }
 
 # Implement
-set_property strategy Congestion_SpreadLogic_low [get_runs impl_1]
+set_property strategy Congestion_SpreadLogic_high [get_runs impl_1]
 launch_runs impl_1 -jobs 12
 wait_on_run impl_1
 
