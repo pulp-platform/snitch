d32a6516cfb072be61ba13e0b5f1951404d1b147
Noah Huetter <noahhuetter@gmail.com>
2022-03-08 09:56:13 +0100

    lint: license headers

--- .gitlab-ci.d/memora_retry.sh
+++ .gitlab-ci.d/memora_retry.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
 
 i=1
 max_attempts=10
