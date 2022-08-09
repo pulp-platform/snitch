aa549133809ffccbf8ea343d72df9867c9e002fd
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2022-08-09 21:28:52 +0200

    [REDO] Update pulp_platform_tech_cells_generic to d630579
    Update code from upstream repository https://github.com/pulp-
    platform/tech_cells_generic.git to revision
    d630579ee989bcef62414373fca3a4854e504bbb
    
    * tc_sram_impl: Add back dependent parameters (Paul Scheffler)
    * tc_sram: Move implementation ports to `tc_sram_impl` wrapper (Paul
      Scheffler)
    * tc_sram: Add implementation stubs to fpga module (Paul Scheffler)
    * tc_sram: Add implementation key and IO (Paul Scheffler)
    * tc_sram: Init performance optimization (#24) (JeanRochCoulon)
    * tc_sram: Improve simulation performance (#23) (Manuel Eggimann)
    
    Signed-off-by: Paul Scheffler <paulsc@iis.ee.ethz.ch>

--- hw/vendor/pulp_platform_tech_cells_generic/src/rtl/tc_sram.sv
+++ hw/vendor/pulp_platform_tech_cells_generic/src/rtl/tc_sram.sv
@@ -33,10 +33,6 @@
 // - ImplKey:     Key by which an instance can refer to a specific implementation (e.g. macro).
 //                May be used to look up additional parameters for implementation (e.g. generator,
 //                line width, muxing) in an external reference, such as a configuration file.
-// - impl_in_t:   Implementation-related inputs, such as pseudo-static macro configuration inputs.
-// - impl_out_t:  Implementation-related outputs. To ensure the behavioral accuracy of this model,
-//                these may *not* have any effects at the behavioral abstraction level.
-// - ImplOutSim:  Static output assumed by `impl_out_t` in behavioral simulation.
 //
 // Ports:
 // - `clk_i`:   Clock
@@ -47,8 +43,6 @@
 // - `wdata_i`: Write data, has to be valid on request
 // - `be_i`:    Byte enable, active high
 // - `rdata_o`: Read data, valid `Latency` cycles after a request with `we_i` low.
-// - `impl_i`:  Implementation-related inputs
-// - `impl_o`:  Implementation-related outputs
 //
 // Behaviour:
 // - Address collision:  When Ports are making a write access onto the same address,
@@ -68,9 +62,6 @@ module tc_sram #(
   parameter              SimInit      = "none",   // Simulation initialization
   parameter bit          PrintSimCfg  = 1'b0,     // Print configuration
   parameter              ImplKey      = "none",   // Reference to specific implementation
-  parameter type         impl_in_t    = logic,    // Type for implementation inputs
-  parameter type         impl_out_t   = logic,    // Type for implementation outputs
-  parameter impl_out_t   ImplOutSim   = 'X,       // Implementation output in simulation
   // DEPENDENT PARAMETERS, DO NOT OVERWRITE!
   parameter int unsigned AddrWidth = (NumWords > 32'd1) ? $clog2(NumWords) : 32'd1,
   parameter int unsigned BeWidth   = (DataWidth + ByteWidth - 32'd1) / ByteWidth, // ceil_div
@@ -80,11 +71,6 @@ module tc_sram #(
 ) (
   input  logic                 clk_i,      // Clock
   input  logic                 rst_ni,     // Asynchronous reset active low
-`ifndef PULP_TC_SRAM_NOIMPL
-  // Implementation-related IO
-  input  impl_in_t             impl_i,
-  output impl_out_t            impl_o,
-`endif
   // input ports
   input  logic  [NumPorts-1:0] req_i,      // request
   input  logic  [NumPorts-1:0] we_i,       // write enable
@@ -95,30 +81,21 @@ module tc_sram #(
   output data_t [NumPorts-1:0] rdata_o     // read data
 );
 
-`ifndef PULP_TC_SRAM_NOIMPL
-  // constant implementation output in behavioral simulation
-  assign impl_o = ImplOutSim;
-`endif
-
   // memory array
   data_t sram [NumWords-1:0];
   // hold the read address when no read access is made
   addr_t [NumPorts-1:0] r_addr_q;
 
   // SRAM simulation initialization
-  data_t [NumWords-1:0] init_val;
+  data_t init_val[NumWords-1:0];
   initial begin : proc_sram_init
     for (int unsigned i = 0; i < NumWords; i++) begin
-      for (int unsigned j = 0; j < DataWidth; j++) begin
-        case (SimInit)
-          "zeros":  init_val[i][j] = 1'b0;
-          "ones":   init_val[i][j] = 1'b1;
-          `ifndef VERILATOR
-          "random": init_val[i][j] = $urandom();
-          `endif
-          default:  init_val[i][j] = 1'bx;
-        endcase
-      end
+      case (SimInit)
+        "zeros":  init_val[i] = {DataWidth{1'b0}};
+        "ones":   init_val[i] = {DataWidth{1'b1}};
+        "random": init_val[i] = {DataWidth{$urandom()}};
+        default:  init_val[i] = {DataWidth{1'bx}};
+      endcase
     end
   end
 
@@ -146,47 +123,83 @@ module tc_sram #(
     end
   end
 
-  // write memory array
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      for (int unsigned i = 0; i < NumWords; i++) begin
-        sram[i] <= init_val[i];
-      end
-      for (int i = 0; i < NumPorts; i++) begin
-        r_addr_q[i] <= {AddrWidth{1'b0}};
-        // initialize the read output register for each port
-        if (Latency != 32'd0) begin
-          for (int unsigned j = 0; j < Latency; j++) begin
-            rdata_q[i][j] <= init_val[{AddrWidth{1'b0}}];
-          end
+  // In case simulation initialization is disabled (SimInit == 'none'), don't assign to the sram
+  // content at all. This improves simulation performance in tools like verilator
+  if (SimInit == "none") begin
+    // write memory array without initialization
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+      if (!rst_ni) begin
+        for (int i = 0; i < NumPorts; i++) begin
+          r_addr_q[i] <= {AddrWidth{1'b0}};
         end
-      end
-    end else begin
-      // read value latch happens before new data is written to the sram
-      for (int unsigned i = 0; i < NumPorts; i++) begin
-        if (Latency != 0) begin
-          for (int unsigned j = 0; j < Latency; j++) begin
-            rdata_q[i][j] <= rdata_d[i][j];
-          end
-        end
-      end
-      // there is a request for the SRAM, latch the required register
-      for (int unsigned i = 0; i < NumPorts; i++) begin
-        if (req_i[i]) begin
-          if (we_i[i]) begin
-            // update value when write is set at clock
-            for (int unsigned j = 0; j < DataWidth; j++) begin
-              if (be_i[i][j/ByteWidth]) begin
-                sram[addr_i[i]][j] <= wdata_i[i][j];
-              end
+      end else begin
+        // read value latch happens before new data is written to the sram
+        for (int unsigned i = 0; i < NumPorts; i++) begin
+          if (Latency != 0) begin
+            for (int unsigned j = 0; j < Latency; j++) begin
+              rdata_q[i][j] <= rdata_d[i][j];
             end
-          end else begin
-            // otherwise update read address for subsequent non request cycles
-            r_addr_q[i] <= addr_i[i];
           end
-        end // if req_i
-      end // for ports
-    end // if !rst_ni
+        end
+        // there is a request for the SRAM, latch the required register
+        for (int unsigned i = 0; i < NumPorts; i++) begin
+          if (req_i[i]) begin
+            if (we_i[i]) begin
+              // update value when write is set at clock
+              for (int unsigned j = 0; j < BeWidth; j++) begin
+                if (be_i[i][j]) begin
+                  sram[addr_i[i]][j*ByteWidth+:ByteWidth] <= wdata_i[i][j*ByteWidth+:ByteWidth];
+                end
+              end
+            end else begin
+              // otherwise update read address for subsequent non request cycles
+              r_addr_q[i] <= addr_i[i];
+            end
+          end // if req_i
+        end // for ports
+      end // if !rst_ni
+    end
+  end else begin
+    // write memory array
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+      if (!rst_ni) begin
+        sram <= init_val;
+        for (int i = 0; i < NumPorts; i++) begin
+          r_addr_q[i] <= {AddrWidth{1'b0}};
+          // initialize the read output register for each port
+          if (Latency != 32'd0) begin
+            for (int unsigned j = 0; j < Latency; j++) begin
+              rdata_q[i][j] <= init_val[{AddrWidth{1'b0}}];
+            end
+          end
+        end
+      end else begin
+        // read value latch happens before new data is written to the sram
+        for (int unsigned i = 0; i < NumPorts; i++) begin
+          if (Latency != 0) begin
+            for (int unsigned j = 0; j < Latency; j++) begin
+              rdata_q[i][j] <= rdata_d[i][j];
+            end
+          end
+        end
+        // there is a request for the SRAM, latch the required register
+        for (int unsigned i = 0; i < NumPorts; i++) begin
+          if (req_i[i]) begin
+            if (we_i[i]) begin
+              // update value when write is set at clock
+              for (int unsigned j = 0; j < BeWidth; j++) begin
+                if (be_i[i][j]) begin
+                  sram[addr_i[i]][j*ByteWidth+:ByteWidth] <= wdata_i[i][j*ByteWidth+:ByteWidth];
+                end
+              end
+            end else begin
+              // otherwise update read address for subsequent non request cycles
+              r_addr_q[i] <= addr_i[i];
+            end
+          end // if req_i
+        end // for ports
+      end // if !rst_ni
+    end
   end
 
 // Validate parameters.
