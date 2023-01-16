a197fd7141875e4ebe8daeeee4b8a7e54a864c08
Noah Huetter <noahhuetter@gmail.com>
2023-01-16 12:28:18 +0100

    [snRuntime] Fix FPU barrier

--- sw/snRuntime/src/ssr.c
+++ sw/snRuntime/src/ssr.c
@@ -3,11 +3,15 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "snrt.h"
 
-/// Synchronize the integer and float pipelines.
+/**
+ * @brief Synchronize the integer and float pipelines by creating a dependency in the scoreboard on
+ * `tmp`
+ *
+ */
 void snrt_fpu_fence() {
-    unsigned tmp;
-    asm volatile(
-        "fmv.x.w %0, fa0\n"
-        "mv      %0, %0"
-        : "+r"(tmp)::);
+  unsigned tmp;
+  asm volatile("fmv.x.w %0, fa0\n"
+               "mv      %0, %0"
+               : "+r"(tmp)::);
+
 }
