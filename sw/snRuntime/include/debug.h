0c36ca9e2e583e72920671ae9c093a4511dbbb9a
Noah Huetter <noahhuetter@gmail.com>
2023-01-16 14:21:07 +0100

    [snRuntime] Debugging unified and programmable

--- sw/snRuntime/include/debug.h
+++ sw/snRuntime/include/debug.h
@@ -3,35 +3,74 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if defined(__SNRT_USE_TRACE)
+extern uint32_t snrt_log_level;
 
-#include "printf.h"
+static inline void snrt_debug_set_loglevel(uint32_t lvl) { snrt_log_level = lvl; };
 
-#define snrt_msg(fmt, x...)                                             \
-    do {                                                                \
-        printf("[\033[35mSNRT(%d,%d)\033[0m] " fmt, snrt_cluster_idx(), \
-               snrt_cluster_core_idx(), ##x);                           \
-    } while (0)
+#define LOG_ERROR 0
+#define LOG_WARN 1
+#define LOG_INFO 2
+#define LOG_DEBUG 3
+#define LOG_TRACE 4
 
-#define SNRT_TRACE_INIT 0
-#define SNRT_TRACE_ALLOC 1
+#if defined(DEBUG)
 
-#define snrt_trace(trace, x...) \
-    do {                        \
-        snrt_msg(x);            \
-    } while (0)
+#define snrt_error(fmt, ...)                                                                       \
+  ({                                                                                               \
+    if (LOG_ERROR <= snrt_log_level)                                                               \
+      snrt_printf("[\033[31msnrt(%d,%d):error:%s\033[0m] " fmt, snrt_cluster_idx(),                \
+                  snrt_cluster_core_idx(), __func__, ##__VA_ARGS__);                               \
+  })
+#define snrt_warn(fmt, ...)                                                                        \
+  ({                                                                                               \
+    if (LOG_WARN <= snrt_log_level)                                                                \
+      snrt_printf("[\033[91msnrt(%d,%d):warn:%s\033[0m] " fmt, snrt_cluster_idx(),                 \
+                  snrt_cluster_core_idx(), __func__, ##__VA_ARGS__);                               \
+  })
+#define snrt_info(fmt, ...)                                                                        \
+  ({                                                                                               \
+    if (LOG_INFO <= snrt_log_level)                                                                \
+      snrt_printf("[\033[33msnrt(%d,%d):info:%s\033[0m] " fmt, snrt_cluster_idx(),                 \
+                  snrt_cluster_core_idx(), __func__, ##__VA_ARGS__);                               \
+  })
+#define snrt_debug(fmt, ...)                                                                       \
+  ({                                                                                               \
+    if (LOG_DEBUG <= snrt_log_level)                                                               \
+      snrt_printf("[\033[35msnrt(%d,%d):debug:%s\033[0m] " fmt, snrt_cluster_idx(),                \
+                  snrt_cluster_core_idx(), __func__, ##__VA_ARGS__);                               \
+  })
+#define snrt_trace(fmt, ...)                                                                       \
+  ({                                                                                               \
+    if (LOG_TRACE <= snrt_log_level)                                                               \
+      snrt_printf("[\033[96msnrt(%d,%d):trace:%s\033[0m] " fmt, snrt_cluster_idx(),                \
+                  snrt_cluster_core_idx(), __func__, ##__VA_ARGS__);                               \
+  })
 
-#else
+#else // #if defined(DEBUG)
 
-#define snrt_trace(x...) \
-    do {                 \
-    } while (0)
+#define snrt_error(x...)                                                                           \
+  do {                                                                                             \
+  } while (0)
+#define snrt_warn(x...)                                                                            \
+  do {                                                                                             \
+  } while (0)
+#define snrt_info(x...)                                                                            \
+  do {                                                                                             \
+  } while (0)
+#define snrt_debug(x...)                                                                           \
+  do {                                                                                             \
+  } while (0)
+#define snrt_trace(x...)                                                                           \
+  do {                                                                                             \
+  } while (0)
 
-#endif  // defined(__SNRT_USE_TRACE)
+#endif // defined(SNRT_DEBUG)
 
 #ifdef __cplusplus
 }
