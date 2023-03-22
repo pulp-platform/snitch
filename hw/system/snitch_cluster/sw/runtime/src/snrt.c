ccaf30babb92382c8317858f845f8c90ad2e3e7c
Luca Colagrande <luca.colagrande3@gmail.com>
2023-03-22 19:19:22 +0100

    snitch_cluster: Adapt to new snRuntime and build system

---
+++ hw/system/snitch_cluster/sw/runtime/src/snrt.c
@@ -0,0 +1,13 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "snrt.h"
+
+#include "alloc.c"
+#include "cls.c"
+#include "cluster_interrupts.c"
+#include "dma.c"
+#include "snitch_cluster_start.c"
+#include "sync.c"
+#include "team.c"
