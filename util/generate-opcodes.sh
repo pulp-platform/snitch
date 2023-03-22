fc6310ba569dbff857daf824e8a74a5205a95f7e
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2023-03-22 11:21:12 +0100

    sw/vendor: Add `riscv-opcodes` (#424)
    * sw/vendor: Add `riscv-opcodes` (#381)
    
    * Add and update vendor inclusion of riscv-opcodes
    
    * Update generate-opcodes.sh to work with new riscv-opcodes folder
    
    This changes generate-opcodes.sh s.t. it is still possible to use as before.
    It includes the updated version of the files generated with the script.
    
    * riscv-opcodes: Update vendor inclusion
    
    * sw/vendor: Disable compressed instructions in riscv-opcodes
    
    ---------
    
    Co-authored-by: oakentling <99972117+oakentling@users.noreply.github.com>
    Co-authored-by: Samuel Riedel <sriedel@iis.ee.ethz.ch>

--- util/generate-opcodes.sh
+++ util/generate-opcodes.sh
@@ -8,7 +8,7 @@ set -e
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 RISCV_OPCODES=$ROOT/sw/vendor/riscv-opcodes
-OPCODES=(opcodes opcodes-dma opcodes-rep opcodes-ssr opcodes-flt-occamy)
+OPCODES=(opcodes-pseudo opcodes-rv32i opcodes-rv64i opcodes-rv32m opcodes-rv64m opcodes-rv32a opcodes-rv64a opcodes-rv32h opcodes-rv64h opcodes-rv32f opcodes-rv64f opcodes-rv32d opcodes-rv64d opcodes-rv32q opcodes-rv64q opcodes-system opcodes-custom opcodes-rv32b_CUSTOM opcodes-dma_CUSTOM opcodes-frep_CUSTOM opcodes-ssr_CUSTOM opcodes-flt-occamy_CUSTOM opcodes-rvv-pseudo)
 
 ###########
 # Banshee #
@@ -21,13 +21,13 @@ cat > $INSTR_RS <<- EOM
 
 EOM
 
-cd $RISCV_OPCODES && cat ${OPCODES[@]} | ./parse-opcodes -rust >> $INSTR_RS
+cd $RISCV_OPCODES && cat ${OPCODES[@]} | ./parse_opcodes -rust >> $INSTR_RS
 rustfmt $INSTR_RS
 
 #######
 # RTL #
 #######
-OPCODES+=(opcodes-ipu)
+OPCODES+=(opcodes-ipu_CUSTOM)
 INSTR_SV=$ROOT/hw/ip/snitch/src/riscv_instr.sv
 
 cat > $INSTR_SV <<- EOM
@@ -37,5 +37,5 @@ cat > $INSTR_SV <<- EOM
 
 EOM
 echo -e "// verilog_lint: waive-start parameter-name-style" >> $INSTR_SV
-cd $RISCV_OPCODES && cat ${OPCODES[@]} | ./parse-opcodes -sverilog >> $INSTR_SV
+cd $RISCV_OPCODES && cat ${OPCODES[@]} | ./parse_opcodes -sverilog >> $INSTR_SV
 echo -e "// verilog_lint: waive-stop parameter-name-style" >> $INSTR_SV
