eddb98a7fefb8b8a163e7046131516c7c1c3aa5f
Tim Fischer <fischeti@iis.ee.ethz.ch>
2023-03-22 14:31:24 +0100

    gemm: Revert FP64 changes

--- sw/applications/src/kernels/gemm.c
+++ sw/applications/src/kernels/gemm.c
@@ -60,107 +60,128 @@ void gemm_fp64_baseline(uint32_t M, uint32_t N, uint32_t K, double* A,
 void gemm_fp64_opt(uint32_t M, uint32_t N, uint32_t K, double* A, uint32_t ldA,
                    uint32_t ta, double* B, uint32_t ldB, uint32_t tb, double* C,
                    uint32_t ldC, const uint32_t* ALPHA, uint32_t setup_SSR) {
-    // Unrolling factor of most inner loop.
-    // Should be at least as high as the FMA delay
-    // for maximum utilization
-    const uint32_t unroll = 4;
+     // Unrolling factor of most inner loop.
+     // Should be at least as high as the FMA delay
+     // for maximum utilization
+     const uint32_t unroll = 8;
 
-    // ALPHA != 0 not supported
-    if (*ALPHA != 0) return;
+     // SSR strides and bounds only have to be configured
+     // once in the beginning
+     if (setup_SSR) {
+         // First matrix is stored in transposed format
+         if (ta) {
+             const uint32_t ssr0_b[4] = {unroll, K, N / unroll, M};
+             const uint32_t ssr0_i[4] = {0, 8 * ldA, 0, 8 * 8};
 
-    // ta == 1 not supported
-    if (ta == 1) return;
+             snrt_ssr_loop_3d(SNRT_SSR_DM0, ssr0_b[1], ssr0_b[2], ssr0_b[3],
+                              ssr0_i[1], ssr0_i[2], ssr0_i[3]);
+             snrt_ssr_repeat(SNRT_SSR_DM0, unroll);
+         } else {
+             const uint32_t ssr0_b[4] = {unroll, K, N / unroll, M};
+             const uint32_t ssr0_i[4] = {0, 8, 0, 8 * ldA};
 
-    // SSR strides and bounds only have to be configured
-    // once in the beginning
-    if (setup_SSR) {
-        const uint32_t ssr0_b[4] = {unroll, K, N / unroll, M};
-        const uint32_t ssr0_i[4] = {0, sizeof(double), 0, sizeof(double) * ldA};
+             snrt_ssr_loop_3d(SNRT_SSR_DM0, ssr0_b[1], ssr0_b[2], ssr0_b[3],
+                              ssr0_i[1], ssr0_i[2], ssr0_i[3]);
+             snrt_ssr_repeat(SNRT_SSR_DM0, unroll);
+         }
 
-        snrt_ssr_loop_3d(SNRT_SSR_DM0, ssr0_b[1], ssr0_b[2], ssr0_b[3],
-                         ssr0_i[1], ssr0_i[2], ssr0_i[3]);
-        snrt_ssr_repeat(SNRT_SSR_DM0, unroll);
+         // Second matrix is stored in transposed format
+         if (tb) {
+             const uint32_t ssr1_b[4] = {unroll, K, N / unroll, M};
+             const uint32_t ssr1_i[4] = {8 * ldB, 8, 8 * ldB * unroll, 0};
 
-        // Second matrix is stored in transposed format
-        if (tb) {
-            const uint32_t ssr1_b[4] = {unroll, K, N / unroll, M};
-            const uint32_t ssr1_i[4] = {sizeof(double) * ldB, sizeof(double),
-                                        sizeof(double) * ldB * unroll, 0};
+             snrt_ssr_loop_4d(SNRT_SSR_DM1, ssr1_b[0], ssr1_b[1], ssr1_b[2],
+                              ssr1_b[3], ssr1_i[0], ssr1_i[1], ssr1_i[2],
+                              ssr1_i[3]);
+         } else {
+             const uint32_t ssr1_b[4] = {unroll, K, N / unroll, M};
+             const uint32_t ssr1_i[4] = {8, 8 * ldB, 8 * unroll, 0};
 
-            snrt_ssr_loop_4d(SNRT_SSR_DM1, ssr1_b[0], ssr1_b[1], ssr1_b[2],
-                             ssr1_b[3], ssr1_i[0], ssr1_i[1], ssr1_i[2],
-                             ssr1_i[3]);
-        } else {
-            const uint32_t ssr1_b[4] = {unroll, K, N / unroll, M};
-            const uint32_t ssr1_i[4] = {sizeof(double), sizeof(double) * ldB,
-                                        sizeof(double) * unroll, 0};
+             snrt_ssr_loop_4d(SNRT_SSR_DM1, ssr1_b[0], ssr1_b[1], ssr1_b[2],
+                              ssr1_b[3], ssr1_i[0], ssr1_i[1], ssr1_i[2],
+                              ssr1_i[3]);
+         }
+     }
 
-            snrt_ssr_loop_4d(SNRT_SSR_DM1, ssr1_b[0], ssr1_b[1], ssr1_b[2],
-                             ssr1_b[3], ssr1_i[0], ssr1_i[1], ssr1_i[2],
-                             ssr1_i[3]);
-        }
+     // SSR start address need to be configured each time
+     snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_4D, A);
+     snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_4D, B);
+     snrt_ssr_enable();
 
-        // Output matrix stream
-        const uint32_t ssr2_b[3] = {unroll, N / unroll, M};
-        const uint32_t ssr2_i[3] = {sizeof(double), sizeof(double) * unroll,
-                                    sizeof(double) * ldC};
-        snrt_ssr_loop_3d(SNRT_SSR_DM2, ssr2_b[0], ssr2_b[1], ssr2_b[2],
-                         ssr2_i[0], ssr2_i[1], ssr2_i[2]);
-    }
+     for (uint32_t m = 0; m < M; m++) {
+         uint32_t n = 0;
+         for (uint32_t n0 = 0; n0 < N / unroll; n0++) {
+             register double c[unroll];
 
-    // SSR start address need to be configured each time
-    snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_4D, A);
-    snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_4D, B);
-    snrt_ssr_write(SNRT_SSR_DM2, SNRT_SSR_3D, C);
-    snrt_ssr_enable();
+             // Load intermediate result
+             if (*ALPHA) {
+                 c[0] = C[m * ldC + n + 0];
+                 c[1] = C[m * ldC + n + 1];
+                 c[2] = C[m * ldC + n + 2];
+                 c[3] = C[m * ldC + n + 3];
+                 c[4] = C[m * ldC + n + 4];
+                 c[5] = C[m * ldC + n + 5];
+                 c[6] = C[m * ldC + n + 6];
+                 c[7] = C[m * ldC + n + 7];
+             } else {
+                 c[0] = 0.0;
+                 c[1] = 0.0;
+                 c[2] = 0.0;
+                 c[3] = 0.0;
+                 c[4] = 0.0;
+                 c[5] = 0.0;
+                 c[6] = 0.0;
+                 c[7] = 0.0;
+             }
 
-    register double c[unroll];
+             asm volatile(
+                 "frep.o %[n_frep], 8, 0, 0 \n"
+                 "fmadd.d %[c0], ft0, ft1, %[c0] \n"
+                 "fmadd.d %[c1], ft0, ft1, %[c1] \n"
+                 "fmadd.d %[c2], ft0, ft1, %[c2] \n"
+                 "fmadd.d %[c3], ft0, ft1, %[c3] \n"
+                 "fmadd.d %[c4], ft0, ft1, %[c4] \n"
+                 "fmadd.d %[c5], ft0, ft1, %[c5] \n"
+                 "fmadd.d %[c6], ft0, ft1, %[c6] \n"
+                 "fmadd.d %[c7], ft0, ft1, %[c7] \n"
+                 : [ c0 ] "+f"(c[0]), [ c1 ] "+f"(c[1]), [ c2 ] "+f"(c[2]),
+                   [ c3 ] "+f"(c[3]), [ c4 ] "+f"(c[4]), [ c5 ] "+f"(c[5]),
+                   [ c6 ] "+f"(c[6]), [ c7 ] "+f"(c[7])
+                 : [ n_frep ] "r"(K - 1)
+                 : "ft0", "ft1");
 
-    for (int i = 0; i < M * N / unroll; i++) {
-        asm volatile(
-            // First iteration is done with fmul.d to
-            // prevent zero initialization
-            "fmul.d %[c0], ft0, ft1 \n"
-            "fmul.d %[c1], ft0, ft1 \n"
-            "fmul.d %[c2], ft0, ft1 \n"
-            "fmul.d %[c3], ft0, ft1 \n"
-            // perform frep loop with fmadd.d
-            "frep.o %[n_frep], 4, 0, 0 \n"
-            "fmadd.d %[c0], ft0, ft1, %[c0] \n"
-            "fmadd.d %[c1], ft0, ft1, %[c1] \n"
-            "fmadd.d %[c2], ft0, ft1, %[c2] \n"
-            "fmadd.d %[c3], ft0, ft1, %[c3] \n"
-            // Store back last result
-            "fmadd.d ft2, ft0, ft1, %[c0] \n"
-            "fmadd.d ft2, ft0, ft1, %[c1] \n"
-            "fmadd.d ft2, ft0, ft1, %[c2] \n"
-            "fmadd.d ft2, ft0, ft1, %[c3] \n"
-            : [ c0 ] "+f"(c[0]), [ c1 ] "+f"(c[1]), [ c2 ] "+f"(c[2]),
-              [ c3 ] "+f"(c[3])
-            : [ n_frep ] "r"((K - 2) - 1)
-            : "ft0", "ft1", "ft2");
-    }
-    snrt_fpu_fence();
-    snrt_ssr_disable();
+             // Store results back
+             C[m * ldC + n + 0] = c[0];
+             C[m * ldC + n + 1] = c[1];
+             C[m * ldC + n + 2] = c[2];
+             C[m * ldC + n + 3] = c[3];
+             C[m * ldC + n + 4] = c[4];
+             C[m * ldC + n + 5] = c[5];
+             C[m * ldC + n + 6] = c[6];
+             C[m * ldC + n + 7] = c[7];
+             n += unroll;
+         }
 
-    // Clean up of leftover columns
-    if (N % unroll) {
-        for (int n = N - (N % unroll); n < N; n++) {
-            for (int m = 0; m < M; m++) {
-                double c = 0.0;
-                if (!tb) {
-                    for (uint32_t k = 0; k < K; k++) {
-                        c += A[k + m * ldA] * B[k * ldB + n];
-                    }
-                } else {
-                    for (uint32_t k = 0; k < K; k++) {
-                        c += A[k + m * ldA] * B[k + n * ldB];
-                    }
-                }
-                C[m * ldC + n] = c;
-            }
-        }
-    }
+         // Clean up of leftover columns
+         snrt_ssr_disable();
+
+         for (; n < N; n++) {
+             double c;
+             if (*ALPHA) {
+                 c = C[m * ldC + n];
+             } else {
+                 c = 0.0;
+             }
+             for (uint32_t k = 0; k < K; k++) {
+                 c += A[k + m * ldA] * B[k + n * ldB];
+             }
+             C[m * ldC + n] = c;
+         }
+
+         snrt_ssr_enable();
+     }
+
+     snrt_ssr_disable();
 }
 
 void gemm_fp32_opt(const uint32_t M, const uint32_t N, const uint32_t K,
