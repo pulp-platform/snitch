43edcb4b860ccc07c83b931f0a28d41548e45c59
Luca Colagrande <luca.colagrande3@gmail.com>
2023-03-22 19:22:10 +0100

    snBlas: Redesign to align with new build system

--- sw/snBLAS/src/axpy.c
+++
@@ -1,6 +0,0 @@
-// Copyright 2020 ETH Zurich and University of Bologna.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-#include <snblas.h>
-
-double snblas_hello() { return 42.3141; }
