8a271c4edf13d43a3ba8c4aeed76de76a29b39c6
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-03-24 03:49:18 +0100

    axi/axi_pkg: Use localparam instead of parameter
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- hw/vendor/pulp_platform_axi/src/axi_pkg.sv
+++ hw/vendor/pulp_platform_axi/src/axi_pkg.sv
@@ -19,28 +19,28 @@
 //! AXI Package
 /// Contains all necessary type definitions, constants, and generally useful functions.
 package axi_pkg;
-    /// AXI Transaction Burst Width.
-  parameter int unsigned BurstWidth  = 32'd2;
+  /// AXI Transaction Burst Width.
+  localparam int unsigned BurstWidth  = 32'd2;
   /// AXI Transaction Response Width.
-  parameter int unsigned RespWidth   = 32'd2;
+  localparam int unsigned RespWidth   = 32'd2;
   /// AXI Transaction Cacheability Width.
-  parameter int unsigned CacheWidth  = 32'd4;
+  localparam int unsigned CacheWidth  = 32'd4;
   /// AXI Transaction Protection Width.
-  parameter int unsigned ProtWidth   = 32'd3;
+  localparam int unsigned ProtWidth   = 32'd3;
   /// AXI Transaction Quality of Service Width.
-  parameter int unsigned QosWidth    = 32'd4;
+  localparam int unsigned QosWidth    = 32'd4;
   /// AXI Transaction Region Width.
-  parameter int unsigned RegionWidth = 32'd4;
+  localparam int unsigned RegionWidth = 32'd4;
   /// AXI Transaction Length Width.
-  parameter int unsigned LenWidth    = 32'd8;
+  localparam int unsigned LenWidth    = 32'd8;
   /// AXI Transaction Size Width.
-  parameter int unsigned SizeWidth   = 32'd3;
+  localparam int unsigned SizeWidth   = 32'd3;
   /// AXI Lock Width.
-  parameter int unsigned LockWidth   = 32'd1;
+  localparam int unsigned LockWidth   = 32'd1;
   /// AXI5 Atomic Operation Width.
-  parameter int unsigned AtopWidth   = 32'd6;
+  localparam int unsigned AtopWidth   = 32'd6;
   /// AXI5 Non-Secure Address Identifier.
-  parameter int unsigned NsaidWidth  = 32'd4;
+  localparam int unsigned NsaidWidth  = 32'd4;
 
   /// AXI Transaction Burst Width.
   typedef logic [BurstWidth-1:0]  burst_t;
