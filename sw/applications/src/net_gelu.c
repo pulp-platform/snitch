712e9c73f048d93c0522363b71498146210e311e
vivianep <vivianep@iis.ee.ethz.ch>
2023-04-20 15:45:50 +0200

    sw: add 3D GELU layer

---
+++ sw/applications/src/net_gelu.c
@@ -0,0 +1,30 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// SW testbench for profiling linear kernels in different
+// floating point precisions (fp64, fp32, fp16), as well as
+// different memory layouts for matrices (transposed/not-transposed)
+// Correctness of results are checked automatically
+
+#include "data_gelu.h"
+#include "math.h"
+#include "network.h"
+#include "gelu_layer.h"
+#include "perf_cnt.h"
+#include "printf.h"
+#include "snrt.h"
+#include "utils.h"
+
+int main() {
+    gelu_l.ifmap = (float*)gelu_ifmap_dram;
+    // gelu_l.result = (float*)gelu_ofmap_dram;
+
+    // checksum = (float*)gelu_checksum;
+
+    gelu_layer(&gelu_l);
+
+    // uint32_t error = check_gelu_layer(&linear_l, (float*)linear_checksum);
+
+    return 0;
+}
