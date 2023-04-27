5896ebbf298d40da8025f1ce0f95bdaadfdb1480
Gianna Paulin <pauling@iis.ee.ethz.ch>
2023-04-27 18:54:02 +0200

    ci: Update Verible from v0.0-807-g10e7c71 to v0.0-3222-gb19cdf44

--- hw/ip/future/src/axi_interleaved_xbar.sv
+++ hw/ip/future/src/axi_interleaved_xbar.sv
@@ -161,23 +161,23 @@ import cf_math_pkg::idx_width;
     default_aw_mst_port_en: assert property(
       @(posedge clk_i) (slv_ports_req_i[i].aw_valid && !slv_ports_resp_o[i].aw_ready)
           |=> $stable(en_default_mst_port_i[i]))
-        else $fatal (1, $sformatf("It is not allowed to change the default mst port\
-                                   enable, when there is an unserved Aw beat. Slave Port: %0d", i));
+        else $fatal (1, $sformatf({"It is not allowed to change the default mst port enable,",
+                                   "when there is an unserved Aw beat. Slave Port: %0d"}, i));
     default_aw_mst_port: assert property(
       @(posedge clk_i) (slv_ports_req_i[i].aw_valid && !slv_ports_resp_o[i].aw_ready)
           |=> $stable(default_mst_port_i[i]))
-        else $fatal (1, $sformatf("It is not allowed to change the default mst port\
-                                   when there is an unserved Aw beat. Slave Port: %0d", i));
+        else $fatal (1, $sformatf({"It is not allowed to change the default mst port",
+                                   "when there is an unserved Aw beat. Slave Port: %0d"}, i));
     default_ar_mst_port_en: assert property(
       @(posedge clk_i) (slv_ports_req_i[i].ar_valid && !slv_ports_resp_o[i].ar_ready)
           |=> $stable(en_default_mst_port_i[i]))
-        else $fatal (1, $sformatf("It is not allowed to change the enable, when\
-                                   there is an unserved Ar beat. Slave Port: %0d", i));
+        else $fatal (1, $sformatf({"It is not allowed to change the enable, when",
+                                   "there is an unserved Ar beat. Slave Port: %0d"}, i));
     default_ar_mst_port: assert property(
       @(posedge clk_i) (slv_ports_req_i[i].ar_valid && !slv_ports_resp_o[i].ar_ready)
           |=> $stable(default_mst_port_i[i]))
-        else $fatal (1, $sformatf("It is not allowed to change the default mst port\
-                                   when there is an unserved Ar beat. Slave Port: %0d", i));
+        else $fatal (1, $sformatf({"It is not allowed to change the default mst port",
+                                   "when there is an unserved Ar beat. Slave Port: %0d"}, i));
     `endif
     `endif
     // pragma translate_on
