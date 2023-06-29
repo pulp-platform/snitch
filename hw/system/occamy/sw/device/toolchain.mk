e8cbfafb0699c681c8354729fdb683ddbd6b7957
Viviane Potocnik <vivianep@iis.ee.ethz.ch>
2023-06-29 11:20:19 +0200

    toolchain: switch to llvm as default and adjust flags

--- hw/system/occamy/sw/device/toolchain.mk
+++ hw/system/occamy/sw/device/toolchain.mk
@@ -15,25 +15,42 @@ DEBUG ?= OFF # ON to turn on debugging symbols
 ###################
 
 # Compiler toolchain
-RISCV_CC      = riscv32-unknown-elf-gcc
-RISCV_AR      = riscv32-unknown-elf-ar
-RISCV_OBJCOPY = riscv32-unknown-elf-objcopy
-RISCV_OBJDUMP = riscv32-unknown-elf-objdump
-RISCV_READELF = riscv32-unknown-elf-readelf
+# RISCV_CC      = riscv32-unknown-elf-gcc
+# RISCV_AR      = riscv32-unknown-elf-ar
+# RISCV_OBJCOPY = riscv32-unknown-elf-objcopy
+# RISCV_OBJDUMP = riscv32-unknown-elf-objdump
+# RISCV_READELF = riscv32-unknown-elf-readelf
+RISCV_CC        ?= clang
+RISCV_LD        ?= lld
+RISCV_AR        ?= llvm-ar
+RISCV_OBJCOPY   ?= llvm-objcopy
+RISCV_OBJDUMP   ?= llvm-objdump
+RISCV_DWARFDUMP ?= llvm-dwarfdump
 
 # Compiler flags
 RISCV_CFLAGS += $(addprefix -I,$(INCDIRS))
+ifeq ($(RISCV_CC), clang)
+RISCV_CFLAGS += -mcpu=snitch
+else 
 RISCV_CFLAGS += -march=rv32imafd
+endif
 RISCV_CFLAGS += -mabi=ilp32d
 RISCV_CFLAGS += -mcmodel=medany
-RISCV_CFLAGS += -mno-fdiv
+# RISCV_CFLAGS += -mno-fdiv
 RISCV_CFLAGS += -ffast-math
 RISCV_CFLAGS += -fno-builtin-printf
 RISCV_CFLAGS += -fno-common
+RISCV_CFLAGS += -menable-experimental-extensions
+RISCV_CFLAGS += -fopenmp
 RISCV_CFLAGS += -O3
 ifeq ($(DEBUG), ON)
 RISCV_CFLAGS += -g
 endif
+
+# Linker flags
+RISCV_LDFLAGS += -fuse-ld=$(RISCV_LD)
+RISCV_LDFLAGS += -nostartfiles
+RISCV_LDFLAGS += -lm
 
 # Archiver flags
 RISCV_ARFLAGS = rcs
