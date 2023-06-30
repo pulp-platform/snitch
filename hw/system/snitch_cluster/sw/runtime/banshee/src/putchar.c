80de9c10ea3d7b6c938377c725803d7368b285b6
Viviane Potocnik <vivianep@iis.ee.ethz.ch>
2023-06-30 10:24:57 +0200

    banshee: add printf deps to banshee

--- hw/system/snitch_cluster/sw/runtime/banshee/src/putchar.c
+++ hw/system/snitch_cluster/sw/runtime/banshee/src/putchar.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include <stdint.h>
 // Provide an implementation for putchar.
 void snrt_putchar(char character) {
     *(volatile uint32_t *)0xF00B8000 = character;
